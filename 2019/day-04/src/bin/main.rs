4b45daee50f75cec095c4670662dcec9034b570e
scotow <contact@scotow.com>
2019-12-05 10:45:27 +0100

    Trim input first, cleaning white spaces

--- 2019/day-04/src/bin/main.rs
+++ 2019/day-04/src/bin/main.rs
@@ -5,9 +5,10 @@ use day_04::range::*;
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
+    input = input.trim().to_string();
 
     let inputs: Vec<u32> = input.split('-')
-        .filter_map(|n| n.parse::<u32>().ok())
+        .map(|n| n.parse::<u32>().unwrap())
         .collect();
 
     let range = Range::new(inputs[0], inputs[1], is_valid_serie);
