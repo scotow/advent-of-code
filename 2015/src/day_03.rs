a352ef777ea91dc37cc53e4405187f286cf05845
Scotow <contact@scotow.com>
2020-12-05 00:01:43 +0100

    Add dirty day 3 year 2015

---
+++ 2015/src/day_03.rs
@@ -0,0 +1,42 @@
+use itertools::Itertools;
+
+#[aoc_generator(day3)]
+pub fn input_generator(input: &str) -> Vec<(i32, i32)> {
+    input.chars()
+        .map(|m| match m {
+            '^' => (0, 1),
+            'v' => (0, -1),
+            '>' => (1, 0),
+            '<' => (-1, 0),
+            _ => unreachable!()
+        })
+        .collect()
+}
+
+#[aoc(day3, part1)]
+pub fn part1(input: &[(i32, i32)]) -> usize {
+    trace(input).iter().unique().count()
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &[(i32, i32)]) -> usize {
+    trace(
+        &input.iter().copied()
+            .step_by(2)
+            .collect_vec()
+    ).iter().copied().chain(
+        trace(&input.iter().copied()
+            .skip(1)
+            .step_by(2)
+            .collect_vec()
+        )).unique().count()
+}
+
+pub fn trace(input: &[(i32, i32)]) -> Vec<(i32, i32)> {
+    input.iter()
+        .scan((0, 0), |(h, v), (mh, mv)| {
+            *h += mh; *v += mv;
+            Some((*h, *v))
+        })
+        .collect()
+}
