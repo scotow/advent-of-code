e944ee8be365b1423c4dd2359ee41d2e019b2fab
Scotow <contact@scotow.com>
2020-12-09 19:44:24 +0100

    [WIP] Day 9

--- 2020/src/day_09.rs
+++ 2020/src/day_09.rs
@@ -1,100 +1,76 @@
 use itertools::Itertools;
-use std::collections::HashSet;
-use Action::*;
-use AlgoResult::*;
 
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum Action {
-    Acc,
-    Jmp,
-    Nop,
+const PREAMBLE: usize = 25;
+
+#[aoc_generator(day9)]
+pub fn input_generator(input: &str) -> Vec<u64> {
+    input.lines().map(|l| l.parse().unwrap()).collect()
 }
 
-impl Action {
-    fn to_fn(&self) -> fn(&mut usize, &mut i32, i32) {
-        match self {
-            Acc => acc,
-            Jmp => jmp,
-            Nop => nop,
-            _ => unreachable!(),
-        }
-    }
+#[aoc(day9, part1)]
+fn part1(input: &Vec<u64>) -> u64 {
+    next_invalid(input).1
 }
 
-#[aoc_generator(day8)]
-pub fn input_generator(input: &str) -> Vec<(Action, i32)> {
-    input.lines()
-        .map(|l| l.split(' ').collect_tuple().unwrap())
-        .map(|(a, p)| (match a {
-            "acc" => Acc,
-            "jmp" => Action::Jmp,
-            "nop" => Action::Nop,
-            _ => unreachable!(),
-        }, p.parse::<i32>().unwrap()))
-        .collect()
-}
-
-#[aoc(day8, part1)]
-fn part1(input: &Vec<(Action, i32)>) -> i32 {
-    match solve(input) {
-        Blocked(r) => r,
-        Terminated(_) => unreachable!(),
-    }
-}
-
-#[aoc(day8, part2)]
-fn part2(input: &Vec<(Action, i32)>) -> i32 {
-    input.iter()
+oc(day9, part2)]
+fn part2(input: &Vec<u64>) -> u64 {
+    let res = input.iter()
         .enumerate()
-        .find_map(|(i, (a, p))| {
-            let mut copy = input.clone();
-            copy[i] = (match a {
-                Acc => Acc,
-                Jmp => Nop,
-                Nop => Jmp,
-            }, *p);
-
-            match solve(&copy) {
-                Blocked(_) => None,
-                Terminated(r) => Some(r),
-            }
+        .skip(PREAMBLE)
+        .filter(|(i, n)| {
+            is_invalid(&input[..=*i])
         })
-        .unwrap()
+        .map(|x| dbg!(x))
+        .tuple_windows()
+        .map(|x| dbg!(x))
+        .filter(|((i1, _), (i2, _))| *i2 == i1 + 1)
+        .take_while(|((i1, _), (i2, _))| *i2 == i1 + 1)
+        .collect_vec();
+    // .flat_map(|((_, n1), (_, n2))| vec![n1, n2].into_iter())
+    // .minmax().into_option().unwrap();
+
+    dbg!(res);
+    // res.0 + res.1
+    0
 }
 
-enum AlgoResult<T> {
-    Blocked(T),
-    Terminated(T),
+// #[aoc(day9, part2)]
+// fn part2(input: &Vec<u64>) -> u64 {
+//     let res = input.iter()
+//         .enumerate()
+//         .skip(PREAMBLE)
+//         .filter(|(i, n)| {
+//             is_invalid(&input[..=*i])
+//         })
+//         .map(|x| dbg!(x))
+//         .tuple_windows()
+//         .map(|x| dbg!(x))
+//         .filter(|((i1, _), (i2, _))| *i2 == i1 + 1)
+//         .take_while(|((i1, _), (i2, _))| *i2 == i1 + 1)
+//         .collect_vec();
+//         // .flat_map(|((_, n1), (_, n2))| vec![n1, n2].into_iter())
+//         // .minmax().into_option().unwrap();
+//
+//     dbg!(res);
+//     // res.0 + res.1
+//     0
+// }
+
+fn next_invalid(input: &[u64]) -> (usize, u64) {
+    let res = input.iter()
+        .enumerate()
+        .skip(PREAMBLE)
+        .find(|(i, n)| {
+            // dbg!(..*i, n);
+            is_invalid(&input[..=*i])
+        }).unwrap();
+    (res.0, *res.1)
 }
 
-fn solve(input: &Vec<(Action, i32)>) -> AlgoResult<i32> {
-    let mut visited = HashSet::<usize>::new();
-    let mut pointer = 0;
-    let mut accumulator = 0;
-
-    loop {
-        if pointer == input.len() {
-            return Terminated(accumulator)
-        }
-        if visited.contains(&pointer) {
-            return Blocked(accumulator);
-        }
-        visited.insert(pointer);
-
-        let (act, param) = input[pointer];
-        act.to_fn()(&mut pointer, &mut accumulator, param);
-    }
-}
-
-fn acc(pointer: &mut usize, acc: &mut i32, param: i32) {
-    *acc += param;
-    *pointer += 1;
-}
-
-fn jmp(pointer: &mut usize, _acc: &mut i32, param: i32) {
-    *pointer = ((*pointer as i32) + param) as usize
-}
-
-fn nop(pointer: &mut usize, _acc: &mut i32, _param: i32) {
-    *pointer += 1;
+fn is_invalid(input: &[u64]) -> bool {
+    let (value, end) = (input[input.len() - 1], input.len() - 1);
+    // dbg!(value, index, input.len(), index-5..index, input[index-5..index].iter().collect_vec());
+    !input[end-PREAMBLE..end].iter()
+        .combinations(2)
+        .any(|v| v[0] + v[1] == value)
 }
