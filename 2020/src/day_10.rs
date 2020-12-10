134c86e224f98d191ce5932af2fca31ea9441779
Scotow <contact@scotow.com>
2020-12-10 14:17:55 +0100

    Day 10 Year 2020

---
+++ 2020/src/day_10.rs
@@ -0,0 +1,31 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
+#[aoc_generator(day10)]
+pub fn input_generator(input: &str) -> Vec<u64> {
+    input.lines().map(|l| l.parse().unwrap()).sorted().collect()
+}
+
+#[aoc(day10, part1)]
+fn part1(input: &Vec<u64>) -> u64 {
+    let mut diffs = HashMap::new();
+    input.iter()
+        .tuple_windows()
+        .for_each(|(n, m)| *diffs.entry(m - n).or_insert(1) += 1);
+
+    diffs.get(&1).unwrap_or(&0) * diffs.get(&3).unwrap_or(&0)
+}
+
+#[aoc(day10, part2)]
+fn part2(input: &Vec<u64>) -> u64 {
+    let mut diffs = HashMap::new();
+    diffs.insert(0, 1);
+
+    input.iter()
+        .for_each(|&a| {
+            diffs.insert(a, (1..=3).map(|i| {
+                diffs.get(&(a - i)).unwrap_or(&0)
+            }).sum());
+        });
+    diffs[&input[input.len() - 1]]
+}
