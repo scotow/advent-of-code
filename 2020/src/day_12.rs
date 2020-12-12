587adbeaa6b587279d7e77bbea797976dd236fb5
Scotow <contact@scotow.com>
2020-12-12 17:08:53 +0100

    Refactor day 12 year 2020

--- 2020/src/day_12.rs
+++ 2020/src/day_12.rs
@@ -1,18 +1,33 @@
 use std::str::FromStr;
 use crate::day_12::Action::*;
 
+trait Ship {
+    fn new() -> Self;
+    fn pos(&self) -> (i32, i32);
+    fn apply(&mut self, mov: Move);
+
+    fn manhattan_distance(&self) -> i32 {
+        let pos = self.pos();
+        pos.0.abs() + pos.1.abs()
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
-struct Ship {
+struct SimpleShip {
     pos: (i32, i32),
     dir: Action,
 }
 
-impl Ship {
-    fn new() -> Ship {
+impl Ship for SimpleShip {
+    fn new() -> Self {
         Self {
             pos: (0, 0),
             dir: East,
         }
+    }
+
+    fn pos(&self) -> (i32, i32) {
+        self.pos
     }
 
     fn apply(&mut self, mov: Move) {
@@ -23,6 +38,44 @@ impl Ship {
             West => self.pos.0 -= mov.value as i32,
             Left | Right => self.dir = Action::from_degree(self.dir.to_degree() + mov.to_degree()),
             Forward => self.apply(Move { action: self.dir, value: mov.value }),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct WaypointShip {
+    pos: (i32, i32),
+    way_point: (i32, i32),
+}
+
+impl Ship for WaypointShip {
+    fn new() -> Self {
+        Self {
+            pos: (0, 0),
+            way_point: (10, 1),
+        }
+    }
+
+    fn pos(&self) -> (i32, i32) {
+        self.pos
+    }
+
+    fn apply(&mut self, mov: Move) {
+        match mov.action {
+            North => self.way_point.1 += mov.value as i32,
+            South => self.way_point.1 -= mov.value as i32,
+            East => self.way_point.0 += mov.value as i32,
+            West => self.way_point.0 -= mov.value as i32,
+            Left | Right => match mov.to_degree() {
+                90 => self.way_point = (-self.way_point.1, self.way_point.0),
+                180 => self.way_point = (-self.way_point.0, -self.way_point.1),
+                270 => self.way_point = (self.way_point.1, -self.way_point.0),
+                _ => unreachable!(),
+            },
+            Forward => self.pos = (
+                self.pos.0 + self.way_point.0 * mov.value as i32,
+                self.pos.1 + self.way_point.1 * mov.value as i32
+            ),
         }
     }
 }
@@ -108,42 +161,6 @@ impl Action {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
-struct ShipWaypoint {
-    pos: (i32, i32),
-    way_point: (i32, i32),
-}
-
-impl ShipWaypoint {
-
-    fn new() -> Self {
-        Self {
-            pos: (0, 0),
-            way_point: (10, 1),
-        }
-    }
-
-    fn apply(&mut self, mov: Move) {
-        match mov.action {
-            North => self.way_point.1 += mov.value as i32,
-            South => self.way_point.1 -= mov.value as i32,
-            East => self.way_point.0 += mov.value as i32,
-            West => self.way_point.0 -= mov.value as i32,
-            Left | Right => match mov.to_degree() {
-                90 => self.way_point = (-self.way_point.1, self.way_point.0),
-                180 => self.way_point = (-self.way_point.0, -self.way_point.1),
-                270 => self.way_point = (self.way_point.1, -self.way_point.0),
-                _ => unreachable!(),
-            },
-            Forward => self.pos = (
-                self.pos.0 + self.way_point.0 * mov.value as i32,
-                self.pos.1 + self.way_point.1 * mov.value as i32
-            ),
-        }
-    }
-
-}
-
 #[aoc_generator(day12)]
 pub fn input_generator(input: &str) -> Vec<Move> {
     input.lines().map(|l| l.parse().unwrap()).collect()
@@ -151,18 +168,18 @@ pub fn input_generator(input: &str) -> Vec<Move> {
 
 #[aoc(day12, part1)]
 fn part1(input: &Vec<Move>) -> i32 {
-    let mut ship = Ship::new();
-    input.iter()
-        .for_each(|&m| ship.apply(m));
-
-    ship.pos.0.abs() + ship.pos.1.abs()
+    solve::<SimpleShip>(&input)
 }
 
 #[aoc(day12, part2)]
 fn part2(input: &Vec<Move>) -> i32 {
-    let mut ship = ShipWaypoint::new();
-    input.iter()
+    solve::<WaypointShip>(&input)
+}
+
+fn solve<S: Ship>(moves: &[Move]) -> i32 {
+    let mut ship = S::new();
+    moves.iter()
         .for_each(|&m| ship.apply(m));
 
-    ship.pos.0.abs() + ship.pos.1.abs()
+    ship.manhattan_distance()
 }
