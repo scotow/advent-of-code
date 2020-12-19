67a689c29107325369957c51132d77bf60129f87
Scotow <contact@scotow.com>
2020-12-19 21:24:06 +0100

    Day 18 v3

--- 2020/src/day_18.rs
+++ 2020/src/day_18.rs
@@ -1,143 +1,71 @@
-use Value::*;
-use std::fmt::{self, Debug, Formatter};
+// use Value::*;
+use Part::*;
 
-enum Value {
+#[derive(Debug, Copy, Clone)]
+enum Part {
+    Empty,
     Number(u64),
-    Opp(fn(u64, u64) -> u64, Box<Value>, Box<Value>),
-}
-
-impl Debug for Value {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Number(n) => {
-                f.debug_tuple("Number")
-                    .field(n)
-                    .finish()
-            },
-            Opp(_, lhs, rhs) => {
-                f.debug_tuple("Opp")
-                    .field(lhs)
-                    .field(rhs)
-                    .finish()
-            }
-        }
-    }
-}
-
-impl Value {
-    fn resolve(&self) -> u64 {
-        match self {
-            Number(n) => *n,
-            Opp(f, lhs, rhs) => f(lhs.resolve(), rhs.resolve()),
-        }
-    }
+    Raw(u8),
 }
 
 #[aoc_generator(day18)]
 fn input_generator(input: &str) -> Vec<Vec<u8>> {
-    input.lines().map(|l| l.replace(' ', "").into_bytes()).collect()
+    input.lines().map(|l| l.replace(' ', "").as_bytes().to_vec()).collect()
 }
 
 #[aoc(day18, part1)]
 fn part1(input: &[Vec<u8>]) -> u64 {
-    // input.iter()
-    //     .map(|l| solve(l))
-    //     .sum()
-    let mut input = input[0].clone();
+    input.iter()
+        .map(|l| solve(l))
+        .sum()
+    // let mut input = input[0].clone();
     // input.reverse();
-    let v = parse(&input).0;
-    dbg!(v);
-
-    0
+    // let v = parse(&input).0;
+    // dbg!(v);
 }
 
-fn parse(input: &[u8]) -> (Value, usize) {
-    // match input[0] {
-    //     b'0'..=b'9' => Number(std::str::from_utf8(input).unwrap().parse().unwrap());
-    //     b'+' =>
-    // }
-    if input.len() == 1 {
-        return (Number(std::str::from_utf8(&input[0..1]).unwrap().parse().unwrap()), 1);
+fn solve(input: &[u8]) -> u64 {
+    dbg!(std::str::from_utf8(&input));
+
+    let mut exp: Vec<Part> = vec![Empty; input.len()];
+    let mut i = 0;
+    while i < input.len() {
+        match input[i] {
+            b'(' => {
+                let end = i + find_close(&input[i..]);
+                exp[i] = Number(solve(&input[i + 1..end - 1]));
+                i = end;
+            },
+            c @ b'0'..=b'9' => {
+                exp[i] = Number((c - b'0') as u64);
+                i += 1;
+            },
+            c @ b'+' | c @ b'*' => {
+                exp[i] = Raw(c);
+                i += 1;
+            },
+            _ => unreachable!(),
+        }
     }
 
-    dbg!(input);
-    let mut i = 0;
-    let lhs = if input[0] == b'(' {
-        let len = find_close(&input[1..]);
-        let (v, n) = parse(&input[1..len]);
-        i += n;
-        v
-    } else {
-        let v = Number(std::str::from_utf8(&input[0..1]).unwrap().parse().unwrap());
-        i += 1;
-        v
-    };
+    let mut res = 0;
+    let mut opp: fn(u64, u64) -> u64 = std::ops::Add::add;
+    exp.iter()
+        .filter(|&&p| !matches!(p, Empty))
+        .for_each(|&p| {
+            match p {
+                Number(n) => res = opp(res, n),
+                Raw(b'+') => opp = std::ops::Add::add,
+                Raw(b'*') => opp = std::ops::Mul::mul,
+                _ => unreachable!(),
+            }
+        });
 
-    dbg!(i);
-    let opp: fn(u64, u64) -> u64 = match input[i] {
-        b'+' => std::ops::Add::add,
-        b'*' => std::ops::Mul::mul,
-        _ => unreachable!(),
-    };
-    i += 1;
-
-    let (rhs, _) = parse(&input[i..]);
-    (Opp(opp, Box::new(lhs), Box::new(rhs)), i)
-
-    // let mut i = 0;
-    // while i < input.len() {
-    // let (lhs, n) = parse(&input);
-    // let opp: fn(u64, u64) -> u64 = match input[0] {
-    //     b'+' => std::ops::Add::add,
-    //     b'*' => std::ops::Mul::mul,
-    // };
-    // i += 1;
-    // let (rhs, n) = parse(&input);
-    // i += n;
-    // // }
-    // if len(input) == 1 {
-    //     return Number(std::str::from_utf8(input).unwrap().parse().unwrap());
-    // }
-    // if input[0] == b'(' {
-    //     let end = find_close(&input[1..]);
-    //     return parse(&input[1..]);
-    // }
-    // match input[1] {
-    //     b'+' => std::ops::Add::add,
-    //     b'*' => std::ops::Mul::mul,
-    // }
+    res
 }
 
-// fn solve(input: &str) -> u64 {
-//     let mut r = 0;
-//     let mut o: fn(u64, u64) -> u64 = std::ops::Add::add;
-//
-//     let mut i = 0;
-//     while i < input.len() {
-//         match input.chars().nth(i).unwrap() {
-//             c @ '0'..='9' => r = o(r, parse_nb(c)),
-//             '+' => o = std::ops::Add::add,
-//             '*' => o = std::ops::Mul::mul,
-//             '(' => {
-//                 let length = find_close(&input[i+1..]);
-//                 // dbg!(&input[i+1..i+length]);
-//                 r = o(r, solve(&input[i+1..i+length]));
-//                 i += length - 1;
-//             }
-//             ')' => (),
-//             _ => unreachable!(),
-//         }
-//         i += 1;
-//     }
-//     r
-// }
-
-// fn parse_nb(c: char) -> u64 {
-//     c.to_string().parse().unwrap()
-// }
-
 fn find_close(input: &[u8]) -> usize {
-    let mut n = 1;
+    let mut n = 0;
     for (i, c) in input.iter().enumerate() {
         n += match c {
             b'(' => 1,
@@ -150,33 +78,3 @@ fn find_close(input: &[u8]) -> usize {
     }
     unreachable!()
 }
-
-// fn find_open(input: &str) -> usize {
-//     let mut n = 1;
-//     for (i, c) in input.chars().enumerate().rev() {
-//         n += match c {
-//             '(' => 1,
-//             ')' => -1,
-//             _ => 0,
-//         };
-//         if n == 0 {
-//             return i + 1
-//         }
-//     }
-//     unreachable!()
-// }
-
-// fn add_parentheses(input: &str) -> String {
-//     let out = input.to_string();
-//     let mut i = 0;
-//     while i < out.len() {
-//         match input.chars().nth(i).unwrap() {
-//             '+' => {
-//                 match input.chars().
-//             },
-//             _ => unreachable!(),
-//         }
-//         i += 1;
-//     }
-//     out
-// }
