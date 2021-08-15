e5e682a717e7dbc2df36399fe272b48ae14bd266
Scotow <contact@scotow.com>
2021-08-15 21:32:24 +0200

    Add 2020 day 20 part 2

--- 2020/src/day_20.rs
+++ 2020/src/day_20.rs
@@ -1,12 +1,16 @@
 use std::collections::HashMap;
 use itertools::Itertools;
+use itertools::iproduct;
 use std::convert::TryInto;
 use std::fmt::{Debug, Formatter, Write};
 use std::cmp::Ordering;
 
-// const IMAGE_SIZE: usize = 3;
-
 const TILE_SIZE: usize = 10;
+const MONSTER: [[bool; 20]; 3] = [
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true,  false],
+    [true,  false, false, false, false, true,  true,  false, false, false, false, true,  true,  false, false, false, false, true,  true,  true ],
+    [false, true,  false, false, true,  false, false, true,  false, false, true,  false, false, true,  false, false, true,  false, false, false],
+];
 
 #[derive(Clone)]
 struct Tile {
@@ -27,12 +31,8 @@ impl Tile {
     }
 
     fn rotated(&self) -> Tile {
-        let mut rotated = Tile {
-            id: self.id,
-            cells: Default::default(),
-        };
-        (0..TILE_SIZE)
-            .cartesian_product(0..TILE_SIZE)
+        let mut rotated = self.clone();
+        iproduct!(0..TILE_SIZE, 0..TILE_SIZE)
             .for_each(|(x, y)| rotated.cells[x][y] = self.cells[y][TILE_SIZE - x - 1]);
         rotated
     }
@@ -84,7 +84,7 @@ impl Side {
         }
     }
 
-    fn add_to(self, pos: (i16, i16)) -> (i16, i16) {
+    fn add_to(self, pos: (isize, isize)) -> (isize, isize) {
         match self {
             Side::Top => (pos.0, pos.1 + 1),
             Side::Bottom => (pos.0, pos.1 - 1),
@@ -112,10 +112,8 @@ fn input_generator(input: &str) -> Vec<Tile> {
         .collect()
 }
 
-#[aoc(day20, part1)]
-fn part1(input: &Vec<Tile>) -> u64 {
-    let mut remaining = input.clone();
-    let mut image = HashMap::with_capacity(input.len());
+fn assemble_image(mut remaining: Vec<Tile>) -> HashMap<(isize, isize), Tile> {
+    let mut image = HashMap::with_capacity(remaining.len());
     image.insert((0, 0), remaining.pop().unwrap());
     loop {
         'tiles: for (&pos, tile) in &image {
@@ -135,22 +133,106 @@ fn part1(input: &Vec<Tile>) -> u64 {
             break;
         }
     }
+    image
+}
 
-    fn find_corner(image: &HashMap<(i16, i16), Tile>, x_cmp: Ordering, y_cmp: Ordering) -> u64 {
-        image.iter()
-            .map(|(&p, t)| (p, t))
-            .reduce(|((x1, y1), t1), ((x2, y2), t2)| {
-                if x1 == x2 || x1.cmp(&x2) == x_cmp && y1 == y2 || y1.cmp(&y2) == y_cmp {
-                    ((x1, y1), t1)
-                } else {
-                    ((x2, y2), t2)
-                }
-            })
-            .unwrap().1.id
+fn find_corner(tiles: &HashMap<(isize, isize), Tile>, x_cmp: Ordering, y_cmp: Ordering) -> ((isize, isize), &Tile) {
+    tiles.iter()
+        .map(|(&p, t)| (p, t))
+        .reduce(|((x1, y1), t1), ((x2, y2), t2)| {
+            if x1 == x2 || x1.cmp(&x2) == x_cmp && y1 == y2 || y1.cmp(&y2) == y_cmp {
+                ((x1, y1), t1)
+            } else {
+                ((x2, y2), t2)
+            }
+        })
+        .unwrap()
+}
+
+#[allow(dead_code)]
+fn print_image(image: &Vec<Vec<bool>>) {
+    for l in image {
+        for &p in l {
+            print!("{}", if p { '#' } else { '.' });
+        }
+        print!("\n");
+    }
+}
+
+fn image_possibilities(image: &Vec<Vec<bool>>) -> Vec<Vec<Vec<bool>>> {
+    fn rotated(image: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+        let mut rotated = image.clone();
+        iproduct!(0..image.len(), 0..image[0].len())
+            .for_each(|(x, y)| rotated[x][y] = image[y][image[0].len() - x - 1]);
+        rotated
     }
 
-    find_corner(&image, Ordering::Less, Ordering::Greater) *
-        find_corner(&image, Ordering::Greater, Ordering::Greater) *
-        find_corner(&image, Ordering::Less, Ordering::Less) *
-        find_corner(&image, Ordering::Greater, Ordering::Less)
+    fn flipped(image: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+        let mut flipped = image.clone();
+        flipped.iter_mut().for_each(|r| r.reverse());
+        flipped
+    }
+
+    (0..4)
+        .scan(image.clone(), |t, _| {
+            let rotated = rotated(t);
+            *t = rotated.clone();
+            Some(vec![flipped(&rotated), rotated])
+        })
+        .flatten()
+        .collect()
+}
+
+
+
+#[aoc(day20, part1)]
+fn part1(input: &Vec<Tile>) -> u64 {
+    let tiles = assemble_image(input.clone());
+    find_corner(&tiles, Ordering::Less, Ordering::Greater).1.id *
+        find_corner(&tiles, Ordering::Greater, Ordering::Greater).1.id *
+        find_corner(&tiles, Ordering::Less, Ordering::Less).1.id *
+        find_corner(&tiles, Ordering::Greater, Ordering::Less).1.id
+}
+
+#[aoc(day20, part2)]
+fn part2(input: &Vec<Tile>) -> usize {
+    let tiles = assemble_image(input.clone());
+    let bottom_left = find_corner(&tiles, Ordering::Less, Ordering::Less).0;
+    let top_right = find_corner(&tiles, Ordering::Greater, Ordering::Greater).0;
+
+    let mut image = vec![
+        vec![false; (top_right.0 - bottom_left.0 + 1) as usize * (TILE_SIZE - 2)];
+        (top_right.1 - bottom_left.1 + 1) as usize * (TILE_SIZE - 2)
+    ];
+    for (t_x, t_y, y, x) in iproduct!(bottom_left.0..=top_right.0, bottom_left.1..=top_right.1, 0..TILE_SIZE - 2, 0..TILE_SIZE - 2) {
+        image
+            [((top_right.1 - bottom_left.1) - -(bottom_left.1 - t_y)) as usize * (TILE_SIZE - 2) + y]
+            [-(bottom_left.0 - t_x) as usize * (TILE_SIZE - 2) + x] = tiles[&(t_x, t_y)].cells[y + 1][x + 1];
+    }
+
+    for mut variant in image_possibilities(&image) {
+        let mut count = 0;
+        'origin: for (y, x) in iproduct!(0..variant.len() - MONSTER.len() + 1, 0..variant[0].len() - MONSTER[0].len() + 1) {
+            for (m_y, m_x) in iproduct!(0..MONSTER.len(), 0..MONSTER[0].len()) {
+                if MONSTER[m_y][m_x] {
+                    if !variant[y + m_y][x + m_x] {
+                        continue 'origin;
+                    }
+                }
+            }
+            for (m_y, m_x) in iproduct!(0..MONSTER.len(), 0..MONSTER[0].len()) {
+                if MONSTER[m_y][m_x] {
+                    variant[y + m_y][x + m_x] = false;
+                }
+            }
+            count += 1;
+        }
+        if count >= 1 {
+            return variant.iter()
+                .map(|l| l.iter().filter(|&&c| c).count())
+                .sum();
+        }
+    }
+
+    unreachable!();
 }
