fe5964e8470566161e99f3bebfb6ba16683cf096
Benjamin Lopez <benjamin.lopez@worldline.com>
2021-08-20 18:00:51 +0200

    WIP year 2020 day 24

---
+++ 2015/src/day_24.rs
@@ -0,0 +1,47 @@
+#[aoc_generator(day24)]
+pub fn input_generator(input: &str) -> Vec<u16> {
+    input.lines()
+        .map(|l| l.parse().unwrap())
+        .collect()
+}
+
+#[aoc(day24, part1)]
+pub fn part1(input: &[u16]) -> u16 {
+    let group_weigth = input.iter().sum::<u16>() / 3;
+    let mut input = input.to_vec();
+    input.reverse();
+    solve(group_weigth, input, Vec::new(), Vec::new(), Vec::new()).unwrap().1
+}
+
+fn solve(group_weigth: u16, mut r: Vec<u16>, g1: Vec<u16>, g2: Vec<u16>, g3: Vec<u16>) -> Option<(usize, u16)> {
+    if r.is_empty() {
+        if g1.iter().sum::<u16>() == group_weigth && g2.iter().sum::<u16>() == group_weigth && g3.iter().sum::<u16>() == group_weigth {
+            return Some((g1.len(), g1.iter().product()));
+        } else {
+            return None;
+        }
+    }
+
+    if g1.iter().sum::<u16>() > group_weigth || g2.iter().sum::<u16>() > group_weigth || g3.iter().sum::<u16>() > group_weigth {
+        return None;
+    }
+
+    let gift = r.pop().unwrap();
+    [
+        {
+            let mut g1 = g1.clone();
+            g1.push(gift);
+            solve(group_weigth, r.clone(), g1, g2.clone(), g3.clone())
+        },
+        {
+            let mut g2 = g2.clone();
+            g2.push(gift);
+            solve(group_weigth, r.clone(), g1.clone(), g2, g3.clone())
+        },
+        {
+            let mut g3 = g3.clone();
+            g3.push(gift);
+            solve(group_weigth, r.clone(), g1.clone(), g2.clone(), g3)
+        }
+    ].iter().filter_map(|&c| c).min()
+}
--- 2015/src/lib.rs
+++ 2015/src/lib.rs
@@ -24,6 +24,7 @@ pub mod day_20;
 pub mod day_21;
 pub mod day_22;
 pub mod day_23;
+pub mod day_24;
 pub mod day_25;
 
 aoc_lib! { year = 2015 }
