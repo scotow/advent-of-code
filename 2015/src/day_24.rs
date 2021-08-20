52e1c068ddcf5d3991c6f745351d6a78f6ac8a2b
Scotow <contact@scotow.com>
2021-08-20 21:58:31 +0200

    Fix typo year 2015 day 24 before trying another solution

--- 2015/src/day_24.rs
+++ 2015/src/day_24.rs
@@ -7,22 +7,22 @@ pub fn input_generator(input: &str) -> Vec<u16> {
 
 #[aoc(day24, part1)]
 pub fn part1(input: &[u16]) -> u16 {
-    let group_weigth = input.iter().sum::<u16>() / 3;
-    let mut input = input.to_vec();
-    input.reverse();
-    solve(group_weigth, input, Vec::new(), Vec::new(), Vec::new()).unwrap().1
+    let group_weight = input.iter().sum::<u16>() / 3;
+    let input = input.to_vec();
+    // input.reverse();
+    solve(group_weight, input, Vec::new(), Vec::new(), Vec::new()).unwrap().1
 }
 
-fn solve(group_weigth: u16, mut r: Vec<u16>, g1: Vec<u16>, g2: Vec<u16>, g3: Vec<u16>) -> Option<(usize, u16)> {
+fn solve(group_weight: u16, mut r: Vec<u16>, g1: Vec<u16>, g2: Vec<u16>, g3: Vec<u16>) -> Option<(usize, u16)> {
     if r.is_empty() {
-        if g1.iter().sum::<u16>() == group_weigth && g2.iter().sum::<u16>() == group_weigth && g3.iter().sum::<u16>() == group_weigth {
+        if g1.iter().sum::<u16>() == group_weight && g2.iter().sum::<u16>() == group_weight && g3.iter().sum::<u16>() == group_weight {
             return Some((g1.len(), g1.iter().product()));
         } else {
             return None;
         }
     }
 
-    if g1.iter().sum::<u16>() > group_weigth || g2.iter().sum::<u16>() > group_weigth || g3.iter().sum::<u16>() > group_weigth {
+    if g1.iter().sum::<u16>() > group_weight || g2.iter().sum::<u16>() > group_weight || g3.iter().sum::<u16>() > group_weight {
         return None;
     }
 
@@ -31,17 +31,17 @@ fn solve(group_weigth: u16, mut r: Vec<u16>, g1: Vec<u16>, g2: Vec<u16>, g3: Vec
         {
             let mut g1 = g1.clone();
             g1.push(gift);
-            solve(group_weigth, r.clone(), g1, g2.clone(), g3.clone())
+            solve(group_weight, r.clone(), g1, g2.clone(), g3.clone())
         },
         {
             let mut g2 = g2.clone();
             g2.push(gift);
-            solve(group_weigth, r.clone(), g1.clone(), g2, g3.clone())
+            solve(group_weight, r.clone(), g1.clone(), g2, g3.clone())
         },
         {
             let mut g3 = g3.clone();
             g3.push(gift);
-            solve(group_weigth, r.clone(), g1.clone(), g2.clone(), g3)
+            solve(group_weight, r.clone(), g1.clone(), g2.clone(), g3)
         }
     ].iter().filter_map(|&c| c).min()
 }
