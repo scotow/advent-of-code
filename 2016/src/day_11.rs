e5cd958e9777be3aad9c2095881c7daa3eb0edc8
Benjamin Lopez <benjamin.lopez@worldline.com>
2021-08-25 17:43:56 +0200

    [WIP] Day 11 year 2016

--- 2016/src/day_11.rs
+++ 2016/src/day_11.rs
@@ -3,7 +3,6 @@ use std::convert::TryInto;
 use Component::*;
 use std::hash::{Hash, Hasher};
 use std::collections::HashSet;
-use std::collections::hash_map::DefaultHasher;
 
 #[derive(Clone, Debug)]
 pub struct Building {
@@ -12,41 +11,66 @@ pub struct Building {
 }
 
 impl Building {
+    fn current_floor(&self) -> &Vec<Component> {
+        &self.floors[self.elevator]
+    }
+
+    fn current_floor_mut(&mut self) -> &mut Vec<Component> {
+        dbg!(self.elevator);
+        &mut self.floors[self.elevator]
+    }
+
     fn is_valid(&self) -> bool {
         self.floors.iter()
             .all(|f| {
-                f.iter().all(|i| {
-                    match i {
-                        Microchip(n) => {
-                            f.contains(&Generator(n.clone())) || f.iter().all(|i| !i.is_generator())
+                f.iter()
+                    .all(|i| {
+                        match i {
+                            Microchip(n) => {
+                                f.contains(&Generator(n.clone())) || f.iter().all(|i| i.is_microchip())
+                            }
+                            Generator(_) => true,
                         }
-                        Generator(_) => true,
-                    }
-                })
-            })
+                    })
+            }) &&
+            !self.current_floor().is_empty()
     }
 
     fn is_done(&self) -> bool {
         self.floors.split_last().unwrap().1.iter().all(|f| f.is_empty())
     }
+
+    fn floor_hash(&self, i: usize) -> u64 {
+        let floor = &self.floors[i];
+        let types = floor.iter().map(|i| i.inner_name()).collect::<HashSet<_>>();
+        let mut hash = 0;
+        for t in types {
+            if floor.contains(&Generator(t.to_owned())) {
+                hash |= 1;
+            }
+            hash <<= 1;
+            if floor.contains(&Microchip(t.to_owned())) {
+                hash |= 1;
+            }
+        }
+        hash
+    }
 }
+
+impl PartialEq for Building {
+    fn eq(&self, other: &Self) -> bool {
+        self.elevator == other.elevator &&
+            (0..self.floors.len())
+                .all(|i| self.floor_hash(i) == other.floor_hash(i))
+    }
+}
+impl Eq for Building {}
 
 impl Hash for Building {
     fn hash<H: Hasher>(&self, state: &mut H) {
         state.write_usize(self.elevator);
-        for floor in self.floors.iter() {
-            let types = floor.iter().map(|i| i.inner_name()).collect::<HashSet<_>>();
-            let mut hash = 0u64;
-            for t in types {
-                if floor.contains(&Generator(t.to_owned())) {
-                    hash |= 1;
-                }
-                hash <<= 1;
-                if floor.contains(&Microchip(t.to_owned())) {
-                    hash |= 1;
-                }
-            }
-            state.write_u64(hash);
+        for i in 0..self.floors.len() {
+            state.write_u64(self.floor_hash(i));
         }
     }
 }
@@ -65,8 +89,8 @@ impl Component {
         }
     }
 
-    fn is_generator(&self) -> bool {
-        matches!(self, Generator(_))
+    fn is_microchip(&self) -> bool {
+        matches!(self, Microchip(_))
     }
 }
 
@@ -86,11 +110,85 @@ pub fn input_generator(input: &str) -> Building {
                 })
                 .collect()
         }).collect_vec().try_into().unwrap();
-    Building { elevator: 0, floors }
+    Building { elevator: 1, floors }
 }
 
 #[aoc(day11, part1)]
 pub fn part1(building: &Building) -> usize {
+    let mut visited = HashSet::new();
+    solve(&mut visited, building.clone(), 0).unwrap()
+}
 
-    0
+fn solve(visited: &mut HashSet<Building>, building: Building, elevator_uses: usize) -> Option<usize> {
+    if visited.contains(&building) {
+        return None;
+    } else {
+        visited.insert(building.clone());
+    }
+    if !building.is_valid() {
+        return None;
+    }
+    if building.is_done() {
+        return Some(elevator_uses);
+    }
+
+    let mut solutions = Vec::new();
+    // Up.
+    if building.elevator < building.floors.len() - 1 {
+        if building.current_floor().len() == 1 {
+            let mut new = building.clone();
+            let moved = new.current_floor_mut().pop().unwrap();
+            new.elevator += 1;
+            new.current_floor_mut().push(moved);
+            solutions.push(solve(visited, new, elevator_uses + 1));
+        } else {
+            for (i, _c) in building.current_floor().iter().enumerate() {
+                let mut new = building.clone();
+                let moved = new.current_floor_mut().remove(i);
+                new.elevator += 1;
+                new.current_floor_mut().push(moved);
+                solutions.push(solve(visited, new, elevator_uses + 1));
+            }
+            for ((i1, _c1), (i2, _c2)) in building.current_floor().iter().enumerate().tuple_combinations() {
+                let mut new = building.clone();
+                let moved2 = new.current_floor_mut().remove(i2);
+                let moved1 = new.current_floor_mut().remove(i1);
+                new.elevator += 1;
+                new.current_floor_mut().push(moved1);
+                new.current_floor_mut().push(moved2);
+                solutions.push(solve(visited, new, elevator_uses + 1));
+            }
+        }
+    }
+    // Down.
+    if building.elevator > 0 {
+        if building.current_floor().len() == 1 {
+            let mut new = building.clone();
+            let moved = new.current_floor_mut().pop().unwrap();
+            new.elevator -= 1;
+            new.current_floor_mut().push(moved);
+            solutions.push(solve(visited, new, elevator_uses + 1));
+        } else {
+            for (i, _c) in building.current_floor().iter().enumerate() {
+                let mut new = building.clone();
+                let moved = new.current_floor_mut().remove(i);
+                new.elevator -= 1;
+                new.current_floor_mut().push(moved);
+                solutions.push(solve(visited, new, elevator_uses + 1));
+            }
+            for ((i1, _c1), (i2, _c2)) in building.current_floor().iter().enumerate().tuple_combinations() {
+                let mut new = building.clone();
+                let moved2 = new.current_floor_mut().remove(i2);
+                let moved1 = new.current_floor_mut().remove(i1);
+                new.elevator -= 1;
+                new.current_floor_mut().push(moved1);
+                new.current_floor_mut().push(moved2);
+                solutions.push(solve(visited, new, elevator_uses + 1));
+            }
+        }
+    }
+
+    solutions.into_iter()
+        .filter_map(|s| s)
+        .min()
 }
