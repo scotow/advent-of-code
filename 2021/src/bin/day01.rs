06a7fe155100fdbb3a7e60774e4ad7f700c7ce11
Scotow <contact@scotow.com>
2021-12-02 12:08:51 +0100

    Refactor day 1 year 2021

--- 2021/src/bin/day01.rs
+++ 2021/src/bin/day01.rs
@@ -3,23 +3,26 @@ advent_of_code_2021::main!();
 fn generator(input: String) -> Vec<u16> {
     input
         .lines()
-        .map(|d| d.parse::<u16>().unwrap())
+        .map(|d| d.parse().unwrap())
         .collect()
 }
 
 fn part_1(input: Vec<u16>) -> usize {
-    input
-        .into_iter()
-        .tuple_windows()
-        .filter(|(d1, d2)| d2 > d1)
-        .count()
+    solve(input)
 }
 
 fn part_2(input: Vec<u16>) -> usize {
+    solve(
+        input
+            .into_iter()
+            .tuple_windows()
+            .map(|(d1, d2, d3)| d1 + d2 + d3)
+    )
+}
+
+fn solve(input: impl IntoIterator<Item=u16>) -> usize {
     input
         .into_iter()
-        .tuple_windows()
-        .map(|(d1, d2, d3)| d1 + d2 + d3)
         .tuple_windows()
         .filter(|(d1, d2)| d2 > d1)
         .count()
