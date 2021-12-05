25154b055c1a2914469c96965c6689d025c7ce5c
Scotow <contact@scotow.com>
2021-12-06 00:19:29 +0100

    Refactor day 5

--- 2021/src/bin/day_05.rs
+++ 2021/src/bin/day_05.rs
@@ -1,46 +1,41 @@
 advent_of_code_2021::main!();
 
-fn generator(input: &str) -> Vec<(u16, u16, u16, u16)> {
+fn generator(input: &str) -> Vec<(i16, i16, i16, i16)> {
     input
         .lines()
         .map(|l| {
             l.split(|c: char| !c.is_numeric())
-                .filter_map(|p| p.parse::<u16>().ok())
+                .filter_map(|p| p.parse().ok())
                 .collect_tuple()
                 .unwrap()
         })
         .collect()
 }
 
-fn part_1(input: Vec<(u16, u16, u16, u16)>) -> usize {
-    input
-        .into_iter()
-        .filter(|(x1, y1, x2, y2)| x1 == x2 || y1 == y2)
-        .map(|(x1, y1, x2, y2)| (x1.min(x2), y1.min(y2), x1.max(x2), y1.max(y2)))
-        .flat_map(|(x1, y1, x2, y2)| (x1..=x2).cartesian_product(y1..=y2))
-        .counts()
-        .into_values()
-        .filter(|&n| n >= 2)
-        .count()
+fn part_1(input: Vec<(i16, i16, i16, i16)>) -> usize {
+    overlaps(
+        input
+            .into_iter()
+            .filter(|(x1, y1, x2, y2)| x1 == x2 || y1 == y2),
+    )
 }
 
-fn part_2(input: Vec<(u16, u16, u16, u16)>) -> usize {
+fn part_2(input: Vec<(i16, i16, i16, i16)>) -> usize {
+    overlaps(input)
+}
+
+fn overlaps(input: impl IntoIterator<Item = (i16, i16, i16, i16)>) -> usize {
     input
         .into_iter()
         .flat_map(|(x1, y1, x2, y2)| {
-            if x1 == x2 || y1 == y2 {
-                let (x1, y1, x2, y2) = (x1.min(x2), y1.min(y2), x1.max(x2), y1.max(y2));
-                (x1..=x2).cartesian_product(y1..=y2).collect_vec()
-            } else {
-                let ((x1, y1), (x2, y2)) = ((x1, y1).min((x2, y2)), (x1, y1).max((x2, y2)));
-                (x1..=x2)
-                    .zip(if y2 > y1 {
-                        (y1..=y2).collect_vec()
-                    } else {
-                        (y2..=y1).rev().collect_vec()
-                    })
-                    .collect_vec()
+            let (mut x, mut y) = (x1, y1);
+            let mut pos = vec![(x, y)];
+            while x != x2 || y != y2 {
+                x += (x2 - x1).signum();
+                y += (y2 - y1).signum();
+                pos.push((x, y));
             }
+            pos
         })
         .counts()
         .into_values()
