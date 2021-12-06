29506df556201e84fb3bc873b1ba619df2060414
Benjamin Lopez <contact@scotow.com>
2021-12-06 15:23:33 +0100

    Refactor day 6

--- 2021/src/bin/day_06.rs
+++ 2021/src/bin/day_06.rs
@@ -1,27 +1,23 @@
 advent_of_code_2021::main!();
 
-fn generator(input: &str) -> Vec<u32> {
+fn generator(input: &str) -> Vec<usize> {
     input.split(',').map(|c| c.parse().unwrap()).collect()
 }
 
-fn part_1(input: Vec<u32>) -> usize {
+fn part_1(input: Vec<usize>) -> usize {
     solve(input, 80)
 }
 
-fn part_2(input: Vec<u32>) -> usize {
+fn part_2(input: Vec<usize>) -> usize {
     solve(input, 256)
 }
 
-fn solve(input: Vec<u32>, days: usize) -> usize {
-    let mut fishes = input.into_iter().counts();
-    for _ in 0..days {
-        let zeros = fishes.remove(&0).unwrap_or(0);
-        *fishes.entry(7).or_insert(0) += zeros;
-        fishes.insert(9, zeros);
-        for i in 1..=9 {
-            let new = fishes.remove(&i).unwrap_or(0);
-            fishes.insert(i - 1, new);
-        }
-    }
-    fishes.values().sum()
+fn solve(input: Vec<usize>, days: usize) -> usize {
+    let mut fishes = [0; 9];
+    input.into_iter().for_each(|f| fishes[f] += 1);
+    (0..days).for_each(|_| {
+        fishes.rotate_left(1);
+        fishes[6] += fishes[8];
+    });
+    fishes.iter().sum()
 }
