62b89e11d404a2af78c1819a40bf4d14c6024994
Benjamin Lopez <contact@scotow.com>
2021-12-07 11:39:18 +0100

    Add day 7 year 2021

---
+++ 2021/src/bin/day_07.rs
@@ -0,0 +1,22 @@
+advent_of_code_2021::main!();
+
+fn generator(input: &str) -> Vec<i32> {
+    input.split(',').map(|c| c.parse().unwrap()).collect()
+}
+
+fn part_1(mut input: Vec<i32>) -> i32 {
+    input.sort();
+    let med = input[input.len() / 2];
+    input.into_iter().map(|n| (med - n).abs()).sum()
+}
+
+fn part_2(input: Vec<i32>) -> i32 {
+    let mean = input.iter().sum::<i32>() / input.len() as i32;
+    input
+        .into_iter()
+        .map(|n| {
+            let d = (mean - n).abs();
+            (d * (d + 1)) / 2
+        })
+        .sum()
+}
