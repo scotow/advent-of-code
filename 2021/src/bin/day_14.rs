0674795dee5614047cd93f324f5de40d61954b48
Scotow <contact@scotow.com>
2021-12-14 23:30:34 +0100

    Clear day 14 ugly iterator

--- 2021/src/bin/day_14.rs
+++ 2021/src/bin/day_14.rs
@@ -25,32 +25,22 @@ fn part_2((start, mapping): (Vec<u8>, HashMap<[u8; 2], u8>)) -> usize {
 }
 
 fn solve(start: Vec<u8>, mapping: HashMap<[u8; 2], u8>, iter: usize) -> usize {
-    let (min, max) = (0..iter)
-        .fold(
-            (
-                start.iter().copied().counts(),
-                start.windows(2).map(|t| [t[0], t[1]]).counts(),
-            ),
-            |(letters, pairs), _| {
-                pairs.into_iter().fold(
-                    (letters, HashMap::new()),
-                    |(mut letters, mut acc), (p, n)| {
-                        if let Some(&new) = mapping.get(&p) {
-                            *acc.entry([p[0], new]).or_default() += n;
-                            *acc.entry([new, p[1]]).or_default() += n;
-                            *letters.entry(new).or_default() += n;
-                        } else {
-                            *acc.entry(p).or_default() += n;
-                        }
-                        (letters, acc)
-                    },
-                )
-            },
-        )
-        .0
-        .into_values()
-        .minmax()
-        .into_option()
-        .unwrap();
+    let mut letters = start.iter().copied().counts();
+    (0..iter).fold(
+        start.windows(2).map(|t| [t[0], t[1]]).counts(),
+        |pairs, _| {
+            pairs.into_iter().fold(HashMap::new(), |mut acc, (p, n)| {
+                if let Some(&new) = mapping.get(&p) {
+                    *acc.entry([p[0], new]).or_default() += n;
+                    *acc.entry([new, p[1]]).or_default() += n;
+                    *letters.entry(new).or_default() += n;
+                } else {
+                    *acc.entry(p).or_default() += n;
+                }
+                acc
+            })
+        },
+    );
+    let (min, max) = letters.into_values().minmax().into_option().unwrap();
     max - min
 }
