274126ee55a7836a8d825649aa820b8fdfded881
Scotow <contact@scotow.com>
2021-12-16 13:00:25 +0100

    Trim whitespace from input

--- 2021/src/lib.rs
+++ 2021/src/lib.rs
@@ -5,7 +5,8 @@ macro_rules! main {
         use itertools::Itertools;
 
         fn main() {
-            let input = generator(include_str!(concat!("../input/", module_path!(), ".txt")));
+            let input =
+                generator(include_str!(concat!("../input/", module_path!(), ".txt")).trim());
             println!("{}", part_1(input.clone()));
             println!("{}", part_2(input));
         }
