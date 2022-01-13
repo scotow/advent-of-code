0866d7a2f32e3deb38b683a9a0392173da8562e0
Scotow <contact@scotow.com>
2022-01-13 19:13:47 +0100

    Add year 2017 day 24

--- 2017/src/bin/day_24.rs
+++ 2017/src/bin/day_24.rs
@@ -13,49 +13,29 @@ fn generator(input: &str) -> HashSet<(u16, u16)> {
 }
 
 fn part_1(input: HashSet<(u16, u16)>) -> u16 {
-    dbg!(&input);
-    solve(input, 0)
+    solve(input, 0, false).0
 }
 
 fn part_2(input: HashSet<(u16, u16)>) -> u16 {
-    solve2(input, 0, 0).0
+    solve(input, 0, true).0
 }
 
-fn solve(links: HashSet<(u16, u16)>, current: u16) -> u16 {
-    // dbg!(current);
-    current
-        + links
-            .iter()
-            .filter(|&&(a, b)| a == current || b == current)
-            .map(|&(a, b)| {
-                let mut new = links.clone();
-                new.remove(&(a, b));
-                if current == a {
-                    a + solve(new, b)
-                } else {
-                    b + solve(new, a)
-                }
-            })
-            .max()
-            .unwrap_or(0)
-}
-
-fn solve2(links: HashSet<(u16, u16)>, current: u16, depth: usize) -> (u16, usize) {
-    // dbg!(current);
-    current
-        + links
-            .iter()
-            .filter(|&&(a, b)| a == current || b == current)
-            .map(|&(a, b)| {
-                let mut new = links.clone();
-                new.remove(&(a, b));
-                if current == a {
-                    let (strength, depth) = solve2(new, b, depth + 1)
-                    (a + solve2(new, b), depth + 1)
-                } else {
-                    (b + solve2(new, a), depth + 1)
-                }
-            })
-            .max_by(|(s1, d1), (s2, d2)| d1.cmp(d2).then(s1.cmp(s2)))
-            .unwrap_or((0, 1))
+fn solve(links: HashSet<(u16, u16)>, current: u16, wants_depth: bool) -> (u16, usize) {
+    links
+        .iter()
+        .filter(|&&(a, b)| a == current || b == current)
+        .map(|&(a, b)| {
+            let mut new = links.clone();
+            new.remove(&(a, b));
+            let (strength, depth) = solve(new, if current == a { b } else { a }, wants_depth);
+            (a + b + strength, depth + 1)
+        })
+        .max_by(|(s1, d1), (s2, d2)| {
+            if wants_depth {
+                d1.cmp(d2).then(s1.cmp(s2))
+            } else {
+                s1.cmp(s2)
+            }
+        })
+        .unwrap_or((0, 1))
 }
