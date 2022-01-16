eadffd8ed4e8944462ede66ef80b6ca2d464a662
Scotow <contact@scotow.com>
2022-01-16 17:11:21 +0100

    Refactor year 2016 day 14

--- 2016/src/bin/day_14.rs
+++ 2016/src/bin/day_14.rs
@@ -5,31 +5,27 @@ fn generator(input: &str) -> &str {
 }
 
 fn part_1(input: &str) -> usize {
-    solve(input, |s, n| {
-        format!("{:x}", md5::compute(format!("{}{}", s, n)))
-    })
+    solve(input, |s, n| to_array(md5::compute(format!("{}{}", s, n))))
 }
 
 fn part_2(input: &str) -> usize {
     solve(input, |s, n| {
-        format!(
-            "{:x}",
+        to_array(
             (0..2016).fold(md5::compute(format!("{}{}", s, n)), |acc, _| {
                 md5::compute(format!("{:x}", acc))
-            })
+            }),
         )
     })
 }
 
-fn solve(input: &str, gen: impl Fn(&str, usize) -> String) -> usize {
-    let mut cache = HashMap::new();
+fn solve(input: &str, gen: impl Fn(&str, usize) -> [u8; 32]) -> usize {
+    let mut cache = HashMap::with_capacity(40_000);
     (0..64).fold(0, |i, _| {
         (i + 1..)
             .find(|&i| {
                 cache
                     .entry(i)
                     .or_insert_with(|| gen(input, i))
-                    .as_bytes()
                     .windows(3)
                     .find_map(|cs| cs.into_iter().all_equal().then(|| cs[0]))
                     .map(|char| {
@@ -37,7 +33,6 @@ fn solve(input: &str, gen: impl Fn(&str, usize) -> String) -> usize {
                             cache
                                 .entry(j)
                                 .or_insert_with(|| gen(input, j))
-                                .as_bytes()
                                 .windows(5)
                                 .any(|cs| cs.into_iter().all(|&c| c == char))
                         })
@@ -46,4 +41,14 @@ fn solve(input: &str, gen: impl Fn(&str, usize) -> String) -> usize {
             })
             .unwrap()
     })
+}
+
+fn to_array(digest: md5::Digest) -> [u8; 32] {
+    digest
+        .0
+        .into_iter()
+        .flat_map(|c| [c >> 4, c & 0xF])
+        .collect_vec()
+        .try_into()
+        .unwrap()
 }
