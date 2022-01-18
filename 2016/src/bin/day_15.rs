cb00e5010fa7eec537b99df746f763369acf3a2a
Scotow <contact@scotow.com>
2022-01-18 01:05:12 +0100

    Add year 2016 day 15

--- 2016/src/bin/day_15.rs
+++ 2016/src/bin/day_15.rs
@@ -3,22 +3,39 @@ advent_of_code_2016::main!();
 fn generator(input: &str) -> Vec<(usize, usize)> {
     input
         .lines()
-        .map(|l| {
-            l.split(&[' ', '.'])
+        .enumerate()
+        .map(|(i, l)| {
+            let (m, n) = l
+                .split(&[' ', '.'])
                 .skip(3)
                 .step_by(8)
                 .map(|n| n.parse().unwrap())
                 .collect_tuple()
-                .unwrap()
+                .unwrap();
+            ((n + i + 1) % m, m)
         })
         .collect()
 }
 
 fn part_1(input: Vec<(usize, usize)>) -> usize {
-    dbg!(input);
-    0
+    solve(input)
 }
 
-fn part_2(input: Vec<(usize, usize)>) -> usize {
-    0
+fn part_2(mut input: Vec<(usize, usize)>) -> usize {
+    input.push((input.len() + 1, 11));
+    solve(input)
+}
+
+fn solve(discs: Vec<(usize, usize)>) -> usize {
+    let (x, m) = discs
+        .into_iter()
+        .reduce(|(mut x, mut m), (y, n)| {
+            while x % n != y {
+                x += m;
+            }
+            m = num_integer::lcm(m, n);
+            (x, m)
+        })
+        .unwrap();
+    m - x
 }
