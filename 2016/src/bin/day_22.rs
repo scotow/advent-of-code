8d5642fc6ad039abfc855cc4e87054162de01bd9
Scotow <contact@scotow.com>
2022-01-21 00:44:12 +0100

    Add year 2016 day 22 part 2

--- 2016/src/bin/day_22.rs
+++ 2016/src/bin/day_22.rs
@@ -1,32 +1,65 @@
+use pathfinding::prelude::dijkstra;
 advent_of_code_2016::main!();
 
-fn generator(input: &str) -> Vec<(u32, u32)> {
+fn generator(input: &str) -> HashMap<(usize, usize), (u32, u32)> {
     input
         .lines()
         .skip(2)
         .map(|l| {
-            let (t, u) = l
+            let (c, t, u) = l
                 .split_whitespace()
-                .skip(1)
-                .take(2)
-                .map(|s| s.trim_end_matches('T').parse().unwrap())
+                .take(3)
+                .map(|s| {
+                    s.trim_start_matches("/dev/grid/node-x")
+                        .trim_end_matches('T')
+                })
                 .collect_tuple()
                 .unwrap();
-            (u, t)
+            (
+                c.split("-y")
+                    .map(|p| p.parse().unwrap())
+                    .collect_tuple()
+                    .unwrap(),
+                (u.parse().unwrap(), t.parse().unwrap()),
+            )
         })
         .collect()
 }
 
-fn part_1(input: Vec<(u32, u32)>) -> usize {
+fn part_1(input: HashMap<(usize, usize), (u32, u32)>) -> usize {
     input
         .iter()
-        .enumerate()
-        .filter(|&(i, &(used, _))| used > 0)
-        .cartesian_product(input.iter().enumerate())
-        .filter(|&((i, &(u1, t1)), (j, &(u2, t2)))| i != j && u1 <= t2 - u2)
+        .filter(|&(_, &(used, _))| used > 0)
+        .cartesian_product(input.iter())
+        .filter(|&((i, &(u1, _)), (j, &(u2, t2)))| i != j && u1 <= t2 - u2)
         .count()
 }
 
-fn part_2(input: Vec<(u32, u32)>) -> usize {
-    2
+fn part_2(grid: HashMap<(usize, usize), (u32, u32)>) -> usize {
+    let max_x = grid.keys().map(|&xy| xy.0).max().unwrap();
+    dijkstra(
+        &grid
+            .iter()
+            .find_map(|(&xy, &(used, _))| (used == 0).then(|| xy))
+            .unwrap(),
+        |&(x, y)| {
+            [
+                (x, y.wrapping_sub(1)),
+                (x + 1, y),
+                (x, y + 1),
+                (x.wrapping_sub(1), y),
+            ]
+            .into_iter()
+            .filter(|dxy| {
+                grid.contains_key(dxy)
+                    && grid[dxy].0 <= grid[&(x, y)].1
+                    && grid[&(x, y)].0 <= grid[dxy].1
+            })
+            .map(|xy| (xy, 1))
+            .collect_vec()
+        },
+        |&xy| xy == (max_x, 0),
+    )
+    .unwrap()
+    .1 + (max_x - 1) * 5
 }
