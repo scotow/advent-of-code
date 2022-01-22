4398ad0891ba093c5c0fe5b94ad53523aebeccbc
Scotow <contact@scotow.com>
2022-01-22 15:57:52 +0100

    Add year 2018 day 3

--- 2018/src/bin/day_03.rs
+++ 2018/src/bin/day_03.rs
@@ -32,7 +32,7 @@ fn part_2(input: Vec<Claim>) -> usize {
                 .iter()
                 .filter(|&&(n2, ..)| n1 != n2)
                 .all(|&(_, x2, y2, w2, h2)| {
-                    !(x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2)
+                    x1 > x2 + w2 || x1 + w1 < x2 || y1 > y2 + h2 || y1 + h1 < y2
                 })
         })
         .unwrap()
