42e5e3113aaa881238257dc6a90152dfd3431b9d
Scotow <contact@scotow.com>
2022-01-29 01:48:27 +0100

    Add year 2018 day 15 part 1

--- 2018/src/bin/day_15.rs
+++ 2018/src/bin/day_15.rs
@@ -1,16 +1,47 @@
-use pathfinding::prelude::dijkstra;
+use pathfinding::prelude::dijkstra_all;
 use std::fmt::Write;
 
 advent_of_code_2018::main!();
 
 type Grid = Vec<Vec<Cell>>;
+type CellMatch = fn(Cell) -> bool;
 
 #[derive(Copy, Clone, PartialEq)]
 enum Cell {
     Wall,
     Empty,
-    Elf,
-    Goblin,
+    Elf(u32),
+    Goblin(u32),
+}
+
+impl Cell {
+    fn is_empty(self) -> bool {
+        matches!(self, Cell::Empty)
+    }
+
+    fn is_elf(self) -> bool {
+        matches!(self, Cell::Elf(_))
+    }
+
+    fn is_goblin(self) -> bool {
+        matches!(self, Cell::Goblin(_))
+    }
+
+    fn hp(self) -> u32 {
+        match self {
+            Cell::Elf(hp) => hp,
+            Cell::Goblin(hp) => hp,
+            _ => unreachable!(),
+        }
+    }
+
+    fn hp_mut(&mut self) -> &mut u32 {
+        match self {
+            Cell::Elf(hp) => hp,
+            Cell::Goblin(hp) => hp,
+            _ => unreachable!(),
+        }
+    }
 }
 
 impl Debug for Cell {
@@ -18,8 +49,8 @@ impl Debug for Cell {
         f.write_char(match self {
             Cell::Wall => '#',
             Cell::Empty => '.',
-            Cell::Elf => 'E',
-            Cell::Goblin => 'G',
+            Cell::Elf(_) => 'E',
+            Cell::Goblin(_) => 'G',
         })
     }
 }
@@ -31,94 +62,166 @@ fn generator(input: &str) -> Grid {
             l.bytes()
                 .map(|c| match c {
                     b'#' => Cell::Wall,
-                    b'.' => Cell::Empty,
-                    b'E' => Cell::Elf,
-                    b'G' => Cell::Goblin,
-                    _ => unreachable!(),
+                    b'E' => Cell::Elf(200),
+                    b'G' => Cell::Goblin(200),
+                    _ => Cell::Empty,
                 })
                 .collect()
         })
         .collect()
 }
 
-fn part_1(grid: Grid) -> u8 {
-    print_grid(&grid);
-    let elves = find_cells(&grid, Cell::Elf);
-    for (ex, ey) in elves {
-        let targets = find_near(&grid, ex, ey, Cell::Goblin);
-        if targets.len() >= 1 {
-            todo!();
+fn part_1(mut grid: Grid) -> u32 {
+    // print_grid(&grid);
+    let mut rounds = 0;
+    loop {
+        let mut entities = find_cells(&grid, Cell::is_elf);
+        entities.extend(find_cells(&grid, Cell::is_goblin));
+        entities.sort_by(|(x1, y1), (x2, y2)| y1.cmp(y2).then(x1.cmp(x2)));
+        let mut died = HashSet::new();
+        for (ex, ey) in entities {
+            if find_cells(&grid, Cell::is_elf).is_empty()
+                || find_cells(&grid, Cell::is_goblin).is_empty()
+            {
+                return rounds
+                    * grid
+                        .into_iter()
+                        .flat_map(|r| r)
+                        .filter(|&c| matches!(c, Cell::Elf(_) | Cell::Goblin(_)))
+                        .map(|c| c.hp())
+                        .sum::<u32>();
+            }
+            if died.contains(&(ex, ey)) {
+                continue;
+            }
+            if let Some(kill) = turn(&mut grid, ex, ey) {
+                died.insert(kill);
+            }
         }
-        let free_spots = find_cells(&grid, Cell::Goblin)
-            .into_iter()
-            .flat_map(|(x, y)| find_near(&grid, x, y, Cell::Empty))
-            .collect_vec();
-        // dbg!(&free_spots);
-        let nearest = free_spots
-            .into_iter()
-            .filter_map(|(tx, ty)| {
-                dijkstra(
-                    &(ex, ey),
-                    |&(x, y)| {
-                        find_near(&grid, x, y, Cell::Empty)
-                            .into_iter()
-                            .map(|xy| (xy, 1usize))
-                            .collect_vec()
-                    },
-                    |&xy| xy == (tx, ty),
-                )
-                    .map(|(path, len)|)
-            })
-            .into_group_map_by(|(path, len)| *len)
-            .into_iter()
-            .min_by_key(|(l, _)| *l)
-            .map(|(_, ps)| ps)
-            .unwrap()
-            .into_iter()
-            .map(|(p, _)| p[1])
-            .collect_vec();
-        dbg!(&nearest);
-        let selected = reading_order(nearest).unwrap();
-        dbg!(&selected);
+        rounds += 1;
     }
-
-    0
 }
 
 fn part_2(_grid: Grid) -> u8 {
     0
 }
 
-fn find_cells(grid: &Grid, kind: Cell) -> Vec<(usize, usize)> {
+fn turn(grid: &mut Grid, cx: usize, cy: usize) -> Option<(usize, usize)> {
+    let enemies = if grid[cy][cx].is_elf() {
+        Cell::is_goblin
+    } else {
+        Cell::is_elf
+    };
+    match attack(grid, cx, cy, enemies) {
+        (true, kill) => return kill,
+        _ => (),
+    }
+    let (cx, cy) = walk(grid, cx, cy, enemies);
+    attack(grid, cx, cy, enemies).1
+}
+
+fn attack(
+    grid: &mut Grid,
+    cx: usize,
+    cy: usize,
+    enemies: CellMatch,
+) -> (bool, Option<(usize, usize)>) {
+    let targets = find_near(&grid, cx, cy, enemies);
+    if targets.is_empty() {
+        return (false, None);
+    }
+    let ((tx, ty), hp) = targets
+        .into_iter()
+        .map(|(x, y)| ((x, y), grid[y][x].hp()))
+        .min_by(|((x1, y1), h1), ((x2, y2), h2)| h1.cmp(h2).then(y1.cmp(y2)).then(x1.cmp(x2)))
+        .unwrap();
+    if hp <= 3 {
+        grid[ty][tx] = Cell::Empty;
+        (true, Some((tx, ty)))
+    } else {
+        *grid[ty][tx].hp_mut() -= 3;
+        (true, None)
+    }
+}
+
+fn walk(grid: &mut Grid, cx: usize, cy: usize, enemies: CellMatch) -> (usize, usize) {
+    let outs = find_near(&grid, cx, cy, Cell::is_empty);
+    let attack_spots = find_cells(&grid, enemies)
+        .into_iter()
+        .flat_map(|(x, y)| find_near(&grid, x, y, Cell::is_empty))
+        .collect_vec();
+    let choices = outs
+        .iter()
+        .flat_map(|&(ox, oy)| {
+            let mut mapping = dijkstra_all(&(ox, oy), |&(x, y)| {
+                find_near(&grid, x, y, Cell::is_empty)
+                    .into_iter()
+                    .map(|xy| (xy, 1usize))
+                    .collect_vec()
+            });
+            mapping.insert((ox, oy), ((ox, oy), 0));
+            attack_spots.iter().filter_map(move |&(ax, ay)| {
+                if let Some(&(_, cost)) = mapping.get(&(ax, ay)) {
+                    Some((cost, ((ax, ay), (ox, oy))))
+                } else {
+                    None
+                }
+            })
+        })
+        .into_group_map();
+    let bests = match choices.into_iter().min_by_key(|&(dist, _)| dist) {
+        Some((_, bests)) => bests,
+        None => return (cx, cy),
+    };
+    let (_, (ox, oy)) = bests
+        .into_iter()
+        .min_by(|&((ax1, ay1), (ox1, oy1)), &((ax2, ay2), (ox2, oy2))| {
+            ay1.cmp(&ay2)
+                .then(ax1.cmp(&ax2))
+                .then(oy1.cmp(&oy2))
+                .then(ox1.cmp(&ox2))
+        })
+        .unwrap();
+    grid[oy][ox] = grid[cy][cx];
+    grid[cy][cx] = Cell::Empty;
+    (ox, oy)
+}
+
+fn find_cells(grid: &Grid, kind: CellMatch) -> Vec<(usize, usize)> {
     grid.iter()
         .enumerate()
         .flat_map(|(y, r)| {
             r.into_iter()
                 .enumerate()
-                .filter_map(move |(x, &c)| (c == kind).then(|| (x, y)))
+                .filter_map(move |(x, &c)| kind(c).then(|| (x, y)))
         })
         .collect()
 }
 
-fn find_near(grid: &Grid, x: usize, y: usize, kind: Cell) -> Vec<(usize, usize)> {
-    [(x, y - 1), (x - 1, y), (x, y + 1), (x + 1, y)]
+fn find_near(grid: &Grid, x: usize, y: usize, kind: CellMatch) -> Vec<(usize, usize)> {
+    [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)]
         .into_iter()
-        .filter(|&(x, y)| grid[y][x] == kind)
+        .filter(|&(x, y)| kind(grid[y][x]))
         .collect()
-}
-
-fn reading_order(iter: impl IntoIterator<Item = (usize, usize)>) -> Option<(usize, usize)> {
-    iter.into_iter()
-        .min_by(|(x1, y1), (x2, y2)| y1.cmp(y2).then(x1.cmp(x2)))
 }
 
 fn print_grid(grid: &Grid) {
     for r in grid {
         println!(
-            "{}",
+            "{}   {}",
             r.into_iter()
                 .map(|&c| format!("{:?}", c))
-                .collect::<String>()
+                .collect::<String>(),
+            r.into_iter()
+                .filter_map(|&c| {
+                    if matches!(c, Cell::Elf(_) | Cell::Goblin(_)) {
+                        Some(format!("{:?}({})", c, c.hp()))
+                    } else {
+                        None
+                    }
+                })
+                .join(", ")
         );
     }
+    println!();
 }
