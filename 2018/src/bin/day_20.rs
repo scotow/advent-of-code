dfd255b37be86d5fff9cb254f2ba7def46d37799
Scotow <contact@scotow.com>
2022-02-07 01:51:02 +0100

    [WIP] Add year 2018 day 20

---
+++ 2018/src/bin/day_20.rs
@@ -0,0 +1,62 @@
+use pathfinding::prelude::dijkstra_all;
+advent_of_code_2018::main!();
+
+type Map = HashMap<(i16, i16), HashSet<(i16, i16)>>;
+
+fn generator(input: &str) -> Map {
+    let mut map = HashMap::new();
+    parse(
+        input[1..input.len() - 1].as_bytes(),
+        &mut 0,
+        &mut map,
+        (0, 0),
+    );
+    map
+}
+
+fn parse(rgx: &[u8], ptr: &mut usize, map: &mut Map, start: (i16, i16)) -> (i16, i16) {
+    let mut pos = start;
+    while *ptr < rgx.len() {
+        *ptr += 1;
+        let delta = match rgx[*ptr - 1] {
+            b'N' => Some((0, -1)),
+            b'E' => Some((1, 0)),
+            b'S' => Some((0, 1)),
+            b'W' => Some((-1, 0)),
+            b'(' => {
+                pos = parse(rgx, ptr, map, pos);
+                None
+            }
+            b'|' => {
+                pos = parse(rgx, ptr, map, pos);
+                None
+            }
+            b')' => return pos,
+            _ => unreachable!(),
+        };
+        if let Some((dx, dy)) = delta {
+            map.entry(pos).or_insert(HashSet::new()).insert((dx, dy));
+            pos.0 += dx;
+            pos.1 += dy;
+        }
+    }
+    pos
+}
+
+fn part_1(map: Map) -> usize {
+    let paths = dijkstra_all(&(0, 0), |&(x, y)| {
+        map.get(&(x, y))
+            .map(|ngs| {
+                ngs.into_iter()
+                    .map(|&(dx, dy)| ((x + dx, y + dy), 1))
+                    .collect_vec()
+            })
+            .unwrap_or_default()
+    });
+    dbg!(&paths);
+    paths.into_values().map(|(_, n)| n).max().unwrap()
+}
+
+fn part_2(_input: Map) -> u8 {
+    0
+}
