e16010180d47759ac083e3f24896b539bc1d0b34
Scotow <contact@scotow.com>
2022-02-15 20:40:45 +0100

    Add year 2019 day 14

--- 2019/src/bin/day_14.rs
+++ 2019/src/bin/day_14.rs
@@ -1,6 +1,7 @@
+use num_integer::Integer;
 advent_of_code_2019::main!();
 
-fn generator(input: &str) -> HashMap<&str, (u32, Vec<(u32, &str)>)> {
+fn generator(input: &str) -> HashMap<&str, (u64, Vec<(u64, &str)>)> {
     input
         .lines()
         .map(|l| {
@@ -10,7 +11,7 @@ fn generator(input: &str) -> HashMap<&str, (u32, Vec<(u32, &str)>)> {
                 .filter(|s| !s.is_empty())
                 .map(|p| {
                     let (n, t) = p.split_once(' ').unwrap();
-                    (n.parse::<u32>().unwrap(), t)
+                    (n.parse::<u64>().unwrap(), t)
                 })
                 .collect_vec();
             let (n, t) = parts.pop().unwrap();
@@ -19,27 +20,40 @@ fn generator(input: &str) -> HashMap<&str, (u32, Vec<(u32, &str)>)> {
         .collect()
 }
 
-fn part_1(receipts: HashMap<&'static str, (u32, Vec<(u32, &'static str)>)>) -> u32 {
-    // dbg!(&input);
-    let mut inventory = HashMap::new();
-    let mut requirements = HashMap::new();
-    requirements.insert("FUEL", 1);
+fn part_1(receipts: HashMap<&str, (u64, Vec<(u64, &str)>)>) -> u64 {
+    solve(&receipts, 1)
+}
 
-    for _ in 0.. {
-        // dbg!(&requirements);
+fn part_2(receipts: HashMap<&str, (u64, Vec<(u64, &str)>)>) -> u64 {
+    let (mut min, mut max) = (1, 1_000_000_000_000);
+    loop {
+        let middle = (min + max).div_ceil(&2);
+        if solve(&receipts, middle) > 1_000_000_000_000 {
+            max = middle - 1;
+        } else {
+            min = middle;
+        }
+        if min == max {
+            return max;
+        }
+    }
+}
+
+fn solve<'a>(receipts: &HashMap<&'a str, (u64, Vec<(u64, &'a str)>)>, fuels: u64) -> u64 {
+    let mut inventory = HashMap::new();
+    let mut requirements = HashMap::from([("FUEL", fuels)]);
+    while requirements.len() > 1 || !requirements.contains_key("ORE") {
         let (&next, &next_n) = requirements
             .iter()
-            .filter(|&(&t, n)| t != "ORE")
+            .filter(|&(&t, _)| t != "ORE")
             .next()
             .unwrap();
-        // dbg!(next);
-
         let (produce, need) = &receipts[&next];
+        let times = next_n.div_ceil(produce);
         for &(n, t) in need {
-            *requirements.entry(t).or_default() += n;
+            *requirements.entry(t).or_default() += n * times;
         }
-        *inventory.entry(next).or_default() += *produce;
-
+        *inventory.entry(next).or_default() += *produce * times;
         for (r, req_n) in requirements.iter_mut() {
             if let Some(inv_n) = inventory.get_mut(r) {
                 let consume = (*req_n).min(*inv_n);
@@ -48,16 +62,6 @@ fn part_1(receipts: HashMap<&'static str, (u32, Vec<(u32, &'static str)>)>) -> u
             }
         }
         requirements.retain(|_, n| *n > 0);
-
-        // dbg!(&inventory, &requirements);
-
-        if requirements.len() == 1 && requirements.contains_key("ORE") {
-            return requirements["ORE"];
-        }
     }
-    0
-}
-
-fn part_2(input: HashMap<&str, (u32, Vec<(u32, &str)>)>) -> u8 {
-    0
+    requirements["ORE"]
 }
