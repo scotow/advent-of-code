0a3458b5556e9316566b163ccb5ea83529614e05
Scotow <contact@scotow.com>
2022-02-16 23:49:41 +0100

    Add year 2019 day 16 part 2

--- 2019/src/bin/day_16.rs
+++ 2019/src/bin/day_16.rs
@@ -5,24 +5,44 @@ fn generator(input: &str) -> Vec<i16> {
 }
 
 fn part_1(input: Vec<i16>) -> String {
+    solve(input, full)
+}
+
+fn part_2(input: Vec<i16>) -> String {
+    solve(
+        (0..10_000)
+            .flat_map(|_| &input)
+            .skip(input[..7].iter().fold(0, |t, &n| t * 10 + n as usize))
+            .copied()
+            .collect(),
+        second_half,
+    )
+}
+
+fn solve(start: Vec<i16>, next: fn(&[i16]) -> Vec<i16>) -> String {
     (0..100)
-        .fold(input, |prev, _| next(&prev))
+        .fold(start, |prev, _| next(&prev))
         .into_iter()
         .take(8)
         .map(|n| (n as u8 + b'0') as char)
         .collect()
 }
 
-fn part_2(input: Vec<i16>) -> u8 {
-    0
-}
-
-fn next(input: &Vec<i16>) -> Vec<i16> {
+fn full(input: &[i16]) -> Vec<i16> {
     (0..input.len())
         .map(|i| {
             input
                 .into_iter()
-                .zip(r_pattern(i + 1))
+                .zip(
+                    chain!(
+                        repeat_n(0, i + 1),
+                        repeat_n(1, i + 1),
+                        repeat_n(0, i + 1),
+                        repeat_n(-1, i + 1),
+                    )
+                    .cycle()
+                    .skip(1),
+                )
                 .map(|(&n, r)| n * r)
                 .sum::<i16>()
                 .abs()
@@ -31,13 +51,10 @@ fn next(input: &Vec<i16>) -> Vec<i16> {
         .collect()
 }
 
-fn r_pattern(n: usize) -> impl Iterator<Item = i16> {
-    chain!(
-        repeat_n(0, n),
-        repeat_n(1, n),
-        repeat_n(0, n),
-        repeat_n(-1, n),
-    )
-    .cycle()
-    .skip(1)
+fn second_half(input: &[i16]) -> Vec<i16> {
+    let mut next = input.to_vec();
+    for i in (0..input.len() - 1).rev() {
+        next[i] = (input[i] + next[i + 1]) % 10;
+    }
+    next
 }
