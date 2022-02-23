26baee83693e09cb3450e3e37aacfbd8113ab763
Scotow <contact@scotow.com>
2022-02-23 02:06:31 +0100

    [WIP] Add map building and item fetching

--- 2019/src/bin/day_25.rs
+++ 2019/src/bin/day_25.rs
@@ -8,12 +8,23 @@ struct Room {
 }
 
 impl Room {
-    fn parse(prog: &mut Program) -> Self {
+    fn parse(prog: &mut Program) -> (Self, String) {
         let info = prog
             .pull_all()
             .into_iter()
             .map(|c| c as u8 as char)
+            .skip_while(|c| c.is_whitespace())
             .collect::<String>();
+
+        let name = info
+            .lines()
+            .next()
+            .unwrap()
+            .trim_matches(['=', ' '].as_slice())
+            .to_owned();
+
+        // println!("{}", &info);
+
         let doors_pos = info.lines().position(|l| l == "Doors here lead:").unwrap();
         let doors = info
             .lines()
@@ -34,13 +45,14 @@ impl Room {
             None
         };
 
-        // println!("{}", &info);
-
-        Self {
-            doors,
-            item,
-            prog: prog.clone(),
-        }
+        (
+            Self {
+                doors,
+                item,
+                prog: prog.clone(),
+            },
+            name,
+        )
     }
 }
 
@@ -50,40 +62,103 @@ fn generator(input: &str) -> Program {
 
 fn part_1(mut prog: Program) -> i64 {
     prog.run();
-    let mut grid = HashMap::from([((0, 0), Room::parse(&mut prog))]);
+    let (start_room, start_name) = Room::parse(&mut prog);
+    let mut map = HashMap::from([(start_name, (start_room, Vec::new()))]);
     loop {
-        let mut next = grid.clone();
-        let prev_len = grid.len();
-        for ((x, y), room) in grid {
-            for door in room.doors {
-                // dbg!(door);
-                let pos = match door.as_ref() {
-                    "north" => (x, y - 1),
-                    "east" => (x + 1, y),
-                    "south" => (x, y + 1),
-                    "west" => (x - 1, y),
-                    _ => unreachable!(),
-                };
-                if next.contains_key(&pos) {
-                    continue;
-                }
+        let mut next = map.clone();
+        for (name, (room, path)) in map.iter() {
+            if name == "Pressure-Sensitive Floor" {
+                continue;
+            }
+            for door in &room.doors {
                 let mut prog = room.prog.clone();
                 prog.push_multiple(door.bytes().chain(once(b'\n')).map_into());
                 prog.run();
-                next.insert(pos, Room::parse(&mut prog));
+                let mut path = path.clone();
+                path.push(door.clone());
+                let (room, name) = Room::parse(&mut prog);
+                if !next.contains_key(&name) {
+                    next.insert(name, (room, path));
+                }
             }
         }
-        grid = next;
-        if grid.len() == prev_len {
+        if next.len() == map.len() {
             break;
         }
+        map = next;
     }
-    dbg!(&grid[&(0, 1)]);
-    // dbg!(grid.values().filter_map(|r| r.item.as_ref()).collect_vec());
+
+    for (room, path) in map.values() {
+        if room
+            .item
+            .as_ref()
+            .map(|item| {
+                [
+                    "photons",
+                    "escape pod",
+                    "molten lava",
+                    "infinite loop",
+                    "giant electromagnet",
+                ]
+                .contains(&item.as_ref())
+            })
+            .unwrap_or(true)
+        {
+            continue;
+        }
+        for door in path {
+            prog.push_multiple(door.bytes().chain(once(b'\n')).map_into());
+        }
+        prog.push_multiple(
+            chain!(
+                "take ".bytes(),
+                room.item.as_ref().unwrap().bytes(),
+                once(b'\n')
+            )
+            .map_into(),
+        );
+        for door in reverse_path(path) {
+            prog.push_multiple(door.bytes().chain(once(b'\n')).map_into());
+        }
+
+        // dbg!(&room.item);
+    }
+
+    prog.push_multiple("inv\n".bytes().map_into());
+
+    prog.run();
+    println!(
+        "{}",
+        prog.pull_all()
+            .into_iter()
+            .map(|c| c as u8 as char)
+            .skip_while(|c| c.is_whitespace())
+            .collect::<String>()
+    );
+
+    // dbg!(map.values().filter_map(|r| r.0.item.as_ref()).collect_vec());
+    // dbg!(&map["Security Checkpoint"]);
+    // dbg!(reverse_path(&map["Security Checkpoint"].1));
 
     0
 }
 
 fn part_2(_: Program) -> &'static str {
     "N/A"
+}
+
+fn reverse_path(path: &[String]) -> Vec<String> {
+    path.into_iter()
+        .rev()
+        .map(|d| {
+            match d.as_ref() {
+                "north" => "south",
+                "east" => "west",
+                "south" => "north",
+                "west" => "east",
+                _ => unreachable!(),
+            }
+            .to_owned()
+        })
+        .collect()
 }
