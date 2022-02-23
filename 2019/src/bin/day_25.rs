1f9770340e2666f4c37ca073412d16cc4833aa03
Benjamin Lopez <contact@scotow.com>
2022-02-23 18:52:52 +0100

    Add year 2019 day 25

--- 2019/src/bin/day_25.rs
+++ 2019/src/bin/day_25.rs
@@ -1,5 +1,7 @@
 advent_of_code_2019::main!();
 
+type Map = HashMap<String, (Room, Vec<String>)>;
+
 #[derive(Clone, Debug)]
 struct Room {
     doors: Vec<String>,
@@ -9,42 +11,19 @@ struct Room {
 
 impl Room {
     fn parse(prog: &mut Program) -> (Self, String) {
-        let info = prog
-            .pull_all()
-            .into_iter()
-            .map(|c| c as u8 as char)
-            .skip_while(|c| c.is_whitespace())
-            .collect::<String>();
-
+        let info = pull_string(prog);
         let name = info
             .lines()
             .next()
             .unwrap()
             .trim_matches(['=', ' '].as_slice())
             .to_owned();
-
-        // println!("{}", &info);
-
-        let doors_pos = info.lines().position(|l| l == "Doors here lead:").unwrap();
-        let doors = info
+        let mut doors = info
             .lines()
-            .skip(doors_pos + 1)
-            .take_while(|l| l.starts_with('-'))
+            .filter(|l| l.starts_with("- "))
             .map(|l| l.trim_start_matches("- ").to_owned())
             .collect_vec();
-
-        let item = if let Some(items_pos) = info.lines().position(|l| l == "Items here:") {
-            Some(
-                info.lines()
-                    .nth(items_pos + 1)
-                    .unwrap()
-                    .trim_start_matches("- ")
-                    .to_owned(),
-            )
-        } else {
-            None
-        };
-
+        let item = info.contains("Items here:").then(|| doors.pop().unwrap());
         (
             Self {
                 doors,
@@ -66,14 +45,10 @@ fn part_1(mut prog: Program) -> i64 {
     let mut map = HashMap::from([(start_name, (start_room, Vec::new()))]);
     loop {
         let mut next = map.clone();
-        for (name, (room, path)) in map.iter() {
-            if name == "Pressure-Sensitive Floor" {
-                continue;
-            }
+        for (_, (room, path)) in map.iter().filter(|(n, _)| n != &"Pressure-Sensitive Floor") {
             for door in &room.doors {
                 let mut prog = room.prog.clone();
-                prog.push_multiple(door.bytes().chain(once(b'\n')).map_into());
-                prog.run();
+                run_cmd(&mut prog, door, true, false);
                 let mut path = path.clone();
                 path.push(door.clone());
                 let (room, name) = Room::parse(&mut prog);
@@ -88,63 +63,70 @@ fn part_1(mut prog: Program) -> i64 {
         map = next;
     }
 
-    for (room, path) in map.values() {
-        if room
-            .item
-            .as_ref()
-            .map(|item| {
-                [
-                    "photons",
-                    "escape pod",
-                    "molten lava",
-                    "infinite loop",
-                    "giant electromagnet",
-                ]
-                .contains(&item.as_ref())
-            })
-            .unwrap_or(true)
-        {
-            continue;
-        }
-        for door in path {
-            prog.push_multiple(door.bytes().chain(once(b'\n')).map_into());
-        }
-        prog.push_multiple(
+    let items = valid_items(&map);
+    let (last, common) = map["Pressure-Sensitive Floor"].1.split_last().unwrap();
+    assert_eq!(&map["Security Checkpoint"].1, common);
+    map.values()
+        .filter(|(room, _)| {
+            room.item
+                .as_ref()
+                .map(|item| items.contains(&item))
+                .unwrap_or(false)
+        })
+        .flat_map(|(room, path)| {
             chain!(
-                "take ".bytes(),
-                room.item.as_ref().unwrap().bytes(),
-                once(b'\n')
+                path.into_iter().cloned(),
+                once(format!("take {}", room.item.as_ref().unwrap().clone())),
+                reverse_path(path),
             )
-            .map_into(),
-        );
-        for door in reverse_path(path) {
-            prog.push_multiple(door.bytes().chain(once(b'\n')).map_into());
-        }
+        })
+        .chain(common.to_vec())
+        .chain(items.iter().map(|i| format!("drop {}", i)))
+        .for_each(|cmd| {
+            run_cmd(&mut prog, cmd, true, true);
+        });
 
-        // dbg!(&room.item);
-    }
-
-    prog.push_multiple("inv\n".bytes().map_into());
-
-    prog.run();
-    println!(
-        "{}",
-        prog.pull_all()
-            .into_iter()
-            .map(|c| c as u8 as char)
-            .skip_while(|c| c.is_whitespace())
-            .collect::<String>()
-    );
-
-    // dbg!(map.values().filter_map(|r| r.0.item.as_ref()).collect_vec());
-    // dbg!(&map["Security Checkpoint"]);
-    // dbg!(reverse_path(&map["Security Checkpoint"].1));
-
-    0
+    items
+        .iter()
+        .powerset()
+        .map(|carrying| {
+            let mut prog = prog.clone();
+            for item in carrying {
+                run_cmd(&mut prog, format!("take {}", item), true, true);
+            }
+            run_cmd(&mut prog, last, true, false);
+            pull_string(&mut prog)
+        })
+        .find(|resp| !resp.contains("lighter") && !resp.contains("heavier"))
+        .map(|resp| {
+            resp.split_whitespace()
+                .filter_map(|w| w.parse().ok())
+                .next()
+                .unwrap()
+        })
+        .unwrap()
 }
 
 fn part_2(_: Program) -> &'static str {
     "N/A"
+}
+
+fn run_cmd(prog: &mut Program, cmd: impl AsRef<str>, run: bool, clear: bool) {
+    prog.push_multiple(cmd.as_ref().bytes().chain(once(b'\n')).map_into());
+    if run {
+        prog.run();
+        if clear {
+            prog.pull_all();
+        }
+    }
+}
+
+fn pull_string(prog: &mut Program) -> String {
+    prog.pull_all()
+        .into_iter()
+        .map(|c| c as u8 as char)
+        .skip_while(|c| c.is_whitespace())
+        .collect()
 }
 
 fn reverse_path(path: &[String]) -> Vec<String> {
@@ -160,5 +142,22 @@ fn reverse_path(path: &[String]) -> Vec<String> {
             }
             .to_owned()
         })
+        .collect()
+}
+
+fn valid_items(map: &Map) -> Vec<String> {
+    map.values()
+        .filter_map(|(r, _)| r.item.as_ref())
+        .filter(|item| {
+            ![
+                "photons",
+                "escape pod",
+                "molten lava",
+                "infinite loop",
+                "giant electromagnet",
+            ]
+            .contains(&item.as_ref())
+        })
+        .cloned()
         .collect()
 }
