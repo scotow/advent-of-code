6118693325e2988d90da3eb3629507773e536376
Scotow <contact@scotow.com>
2022-02-24 21:18:24 +0100

    Add year 2019 day 22

--- 2019/src/bin/day_22.rs
+++ 2019/src/bin/day_22.rs
@@ -47,6 +47,45 @@ fn part_1(input: Vec<Shuffle>) -> usize {
     deck.into_iter().position(|c| c == 2019).unwrap()
 }
 
-fn part_2(input: Vec<Shuffle>) -> u8 {
-    0
+fn part_2(input: Vec<Shuffle>) -> i128 {
+    let m = 119_315_717_514_047;
+    let mut a = 1;
+    let mut b = 0;
+    for s in input.into_iter().rev() {
+        match s {
+            Shuffle::Reverse => {
+                a = a * -1;
+                b = b * -1 - 1;
+            }
+            Shuffle::Cut(n) => {
+                b = (b + n as i128) % m;
+            }
+            Shuffle::Deal(n) => {
+                a = (a * mod_inverse(n as i128, m)) % m;
+                b = (b * mod_inverse(n as i128, m)) % m;
+            }
+        };
+    }
+    let n = 101_741_582_076_661;
+    (2020 * mod_e(a, n, m) % m + (b * ((mod_e(a, n, m) - 1) * mod_inverse(a - 1, m) % m))) % m
+}
+
+fn mod_e(a: i128, mut e: i128, m: i128) -> i128 {
+    let mut result = 1;
+    let mut base = a % m;
+    loop {
+        if e <= 0 {
+            break;
+        }
+        if e % 2 == 1 {
+            result = (result * base) % m;
+        }
+        e >>= 1;
+        base = (base * base) % m;
+    }
+    result
+}
+
+fn mod_inverse(a: i128, m: i128) -> i128 {
+    mod_e(a, m - 2, m)
 }
