53855c62ca4042df9581e008273db4c52411bd37
Benjamin Lopez <contact@scotow.com>
2022-12-02 15:58:41 +0100

    More math for year 2022 day 02

--- 2022/src/bin/day_02.rs
+++ 2022/src/bin/day_02.rs
@@ -11,7 +11,7 @@ fn part_1(input: Vec<(u8, u8)>) -> u32 {
     run(input, |_, y| y)
 }
 
-fn part_2(mut input: Vec<(u8, u8)>) -> u32 {
+fn part_2(input: Vec<(u8, u8)>) -> u32 {
     run(input, |h, r| (h + r + 2) % 3)
 }
 
@@ -20,11 +20,7 @@ fn run(plays: Vec<(u8, u8)>, mut alt: impl FnMut(u8, u8) -> u8) -> u32 {
         .into_iter()
         .map(|(h, mut y)| {
             y = alt(h, y);
-            (y + 1
-                + matches!((y, h), (1, 0) | (2, 1) | (0, 2))
-                    .then_some(6)
-                    .unwrap_or(0)
-                + (y == h).then_some(3).unwrap_or(0)) as u32
+            (y + 1 + (y + 4 - h) % 3 * 3) as u32
         })
         .sum()
 }
