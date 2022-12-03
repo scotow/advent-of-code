9803d8d4fdf268602e1f956be6fc027c6c49ee1c
scotow <contact@scotow.com>
2022-12-03 15:48:27 +0100

    Add year 2022 day 03

---
+++ 2022/src/bin/day_03.rs
@@ -0,0 +1,28 @@
+advent_of_code_2022::main!();
+
+fn generator(input: &'static str) -> Vec<&'static [u8]> {
+    input.lines().map(|l| l.as_bytes()).collect()
+}
+
+fn part_1(input: Vec<&'static [u8]>) -> u32 {
+    input
+        .into_iter()
+        .map(|b| common(b.chunks_exact(b.len() / 2)))
+        .sum()
+}
+
+fn part_2(input: Vec<&'static [u8]>) -> u32 {
+    input.chunks_exact(3).map(common).sum()
+}
+
+fn common(badges: impl IntoIterator<Item = impl AsRef<[u8]>>) -> u32 {
+    1 + badges
+        .into_iter()
+        .map(|b| HashSet::from_iter(b.as_ref().iter().copied()))
+        .reduce(|b1, b2| b1.intersection(&b2).copied().collect::<HashSet<_>>())
+        .unwrap()
+        .into_iter()
+        .next()
+        .map(|n| n.checked_sub(b'a').unwrap_or_else(|| n - b'A' + 26))
+        .unwrap() as u32
+}
