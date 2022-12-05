350ed34ec54a4ce67de6537c36b5b3b2c9ac4748
Benjamin Lopez <contact@scotow.com>
2022-12-05 13:38:10 +0100

    Clean y22d05

--- 2022/src/bin/day_05.rs
+++ 2022/src/bin/day_05.rs
@@ -2,18 +2,14 @@ advent_of_code_2022::main!();
 
 fn generator(input: &str) -> (Vec<Vec<u8>>, Vec<(usize, usize, usize)>) {
     let (init, ops) = input.split_once("\n\n").unwrap();
-    let mut stacks = repeat_n(
-        Vec::new(),
-        (init.lines().map(|l| l.len()).max().unwrap() + 1) / 4,
-    )
-    .collect_vec();
-    for l in init.lines().dropping_back(1) {
+    let mut stacks = vec![Vec::new(); (init.lines().map(|l| l.len()).max().unwrap() + 1) / 4];
+    for l in init.lines().dropping_back(1).rev() {
         l.bytes()
             .skip(1)
             .step_by(4)
             .enumerate()
-            .filter(|&(_, c)| c != b' ')
-            .for_each(|(i, c)| stacks[i].insert(0, c));
+            .filter(|&(_, c)| c.is_ascii_alphabetic())
+            .for_each(|(i, c)| stacks[i].push(c));
     }
     (
         stacks,
@@ -49,6 +45,6 @@ where
     stacks
         .into_iter()
         .filter_map(|s| s.last().copied())
-        .map(|b| b as char)
+        .map_into::<char>()
         .collect()
 }
