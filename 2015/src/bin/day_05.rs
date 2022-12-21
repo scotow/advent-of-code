d31613833427b404537d95386de305ac71ceb74c
Benjamin Lopez <contact@scotow.com>
2022-12-21 19:05:53 +0100

    Golf/clear y15d5

--- 2015/src/bin/day_05.rs
+++ 2015/src/bin/day_05.rs
@@ -1,32 +1,29 @@
 advent_of_code_2015::main!();
 
-fn generator(input: &str) -> &str {
+fn generator(input: &str) -> Vec<&str> {
+    input.lines().collect()
+}
+
+fn part_1(input: Vec<&str>) -> usize {
     input
-}
-
-fn part_1(input: &str) -> usize {
-    input.lines().filter(|l| is_nice_string_1(l)).count()
-}
-
-fn part_2(input: &str) -> usize {
-    input.lines().filter(|l| is_nice_string_2(l)).count()
-}
-
-fn is_nice_string_1(input: &str) -> bool {
-    input
-        .chars()
-        .filter(|c| ['a', 'e', 'i', 'o', 'u'].contains(c))
+        .into_iter()
+        .filter(|l| {
+            l.bytes()
+                .filter(|c| [b'a', b'e', b'i', b'o', b'u'].contains(c))
+                .count()
+                >= 3
+                && l.bytes().tuple_windows().any(|(a, b)| a == b)
+                && ["ab", "cd", "pq", "xy"].into_iter().all(|s| !l.contains(s))
+        })
         .count()
-        >= 3
-        && input.chars().tuple_windows().any(|(a, b)| a == b)
-        && ["ab", "cd", "pq", "xy"].iter().all(|&s| !input.contains(s))
 }
 
-fn is_nice_string_2(input: &str) -> bool {
+fn part_2(input: Vec<&str>) -> usize {
     input
-        .chars()
-        .tuple_windows::<(_, _)>()
-        .enumerate()
-        .any(|(i, (a, b))| input[(i + 2)..].contains(&[a, b].iter().collect::<String>()))
-        && input.chars().tuple_windows().any(|(x, _, z)| x == z)
+        .into_iter()
+        .filter(|l| {
+            (0..l.len() - 1).any(|i| l[i + 2..].contains(&l[i..i + 2]))
+                && l.bytes().tuple_windows().any(|(x, _, z)| x == z)
+        })
+        .count()
 }
