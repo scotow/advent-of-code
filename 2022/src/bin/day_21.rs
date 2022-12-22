291312af6b256243f0fb75a59faf4ba1f7000573
scotow <contact@scotow.com>
2022-12-22 01:44:25 +0100

    Add y22d21

--- 2022/src/bin/day_21.rs
+++ 2022/src/bin/day_21.rs
@@ -1,8 +1,8 @@
 advent_of_code_2022::main!();
 
-type Map<'a> = HashMap<&'a str, Either<i64, (&'a str, &'a str, fn(i64, i64) -> i64)>>;
+type Tree<'a> = HashMap<&'a str, Either<i64, (&'a str, &'a str, fn(i64, i64) -> i64)>>;
 
-fn generator(input: &str) -> Map {
+fn generator(input: &str) -> Tree {
     input
         .lines()
         .map(|l| {
@@ -26,37 +26,67 @@ fn generator(input: &str) -> Map {
         .collect()
 }
 
-fn part_1(input: Map) -> i64 {
-    resolve(&input, "root")
+fn part_1(tree: Tree) -> i64 {
+    resolve(&tree, "root")
 }
 
-fn part_2(mut input: Map) -> i64 {
-    let other = *input
-        .iter()
-        .find_map(|(_, v)| match v {
-            Either::Right(("humn", o, _)) => Some(o),
-            Either::Right((o, "humn", _)) => Some(o),
-            _ => None,
-        })
-        .unwrap();
-    dbg!(other);
-    dbg!(resolve(&input, other));
+fn part_2(mut tree: Tree) -> i64 {
+    let (left, right, _) = tree["root"].unwrap_right();
+    let (mut target, mut to_resolve) = if contains(&tree, left, "humn") {
+        (resolve(&tree, right), left)
+    } else {
+        (resolve(&tree, left), right)
+    };
 
-    // let (k1, k2, _) = input["root"].unwrap_right();
-    // for i in 1.. {
-    //     *input.get_mut("humn").unwrap() = Either::Left(i);
-    //     if resolve(&input, k1) == resolve(&input, k2) {
-    //         return i;
-    //     }
-    // }
-    // unreachable!()
-
-    0
+    while to_resolve != "humn" {
+        let (a, b, ops) = tree[to_resolve].unwrap_right();
+        if a == "humn" || contains(&tree, a, "humn") {
+            to_resolve = a;
+            let b = resolve(&tree, b);
+            if ops == i64::div {
+                target *= b;
+            } else if ops == i64::mul {
+                target /= b;
+            } else if ops == i64::add {
+                target -= b;
+            } else if ops == i64::sub {
+                target += b;
+            } else {
+                unreachable!();
+            }
+        } else {
+            if ops == i64::div {
+                tree.insert(to_resolve, Either::Right((b, a, i64::mul)));
+                target = resolve(&tree, a);
+            } else if ops == i64::mul {
+                target /= resolve(&tree, a);
+                to_resolve = b;
+            } else if ops == i64::add {
+                target -= resolve(&tree, a);
+                to_resolve = b;
+            } else if ops == i64::sub {
+                target = -target + resolve(&tree, a);
+                to_resolve = b;
+            } else {
+                unreachable!();
+            }
+        };
+    }
+    target
 }
 
-fn resolve(mapping: &Map, key: &str) -> i64 {
-    match mapping[key] {
+fn resolve(tree: &Tree, key: &str) -> i64 {
+    match tree[key] {
         Either::Left(n) => n,
-        Either::Right((k1, k2, op)) => op(resolve(mapping, k1), resolve(mapping, k2)),
+        Either::Right((k1, k2, op)) => op(resolve(tree, k1), resolve(tree, k2)),
+    }
+}
+
+fn contains(tree: &Tree, root: &str, key: &str) -> bool {
+    match tree[root] {
+        Either::Left(_) => false,
+        Either::Right((s1, s2, _)) => {
+            s1 == key || s2 == key || contains(tree, s1, key) || contains(tree, s2, key)
+        }
     }
 }
