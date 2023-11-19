1f57000e923ff9a247dc4aa8eb169a4a27ade587
scotow <contact@scotow.com>
2023-11-19 19:10:19 +0100

    Prepare for year 2023

---
+++ 2023/src/bin/day_11.rs
@@ -0,0 +1,13 @@
+advent_of_code_2023::main!();
+
+fn generator(input: &str) -> &str {
+    input
+}
+
+fn part_1(input: &str) -> &str {
+    input
+}
+
+fn part_2(input: &str) -> &str {
+    input
+}
