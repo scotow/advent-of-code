47d98371834ff0032278ece8770b0e4d338ad22c
scotow <contact@scotow.com>
2023-12-02 12:38:14 +0100

    Add 2023 day 02

--- 2023/src/bin/day_02.rs
+++ 2023/src/bin/day_02.rs
@@ -1,13 +1,35 @@
 advent_of_code_2023::main!();
 
-fn generator(input: &str) -> &str {
+fn generator(input: &str) -> Vec<HashMap<&str, u32>> {
     input
+        .lines()
+        .map(|g| {
+            g.split_once(": ")
+                .unwrap()
+                .1
+                .split("; ")
+                .flat_map(|d| {
+                    d.split(", ").map(|c| {
+                        let (n, c) = c.split_once(' ').unwrap();
+                        (c, n.parse().unwrap())
+                    })
+                })
+                .sorted_by_key(|(_c, n)| *n)
+                .collect()
+        })
+        .collect()
 }
 
-fn part_1(input: &str) -> &str {
+fn part_1(input: Vec<HashMap<&str, u32>>) -> usize {
     input
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, g)| {
+            (g["red"] <= 12 && g["green"] <= 13 && g["blue"] <= 14).then_some(i + 1)
+        })
+        .sum()
 }
 
-fn part_2(input: &str) -> &str {
-    input
+fn part_2(input: Vec<HashMap<&str, u32>>) -> u32 {
+    input.iter().map(|ds| ds.values().product::<u32>()).sum()
 }
