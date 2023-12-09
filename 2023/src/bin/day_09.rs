d657f98920485402dc34a26bcd6e9eaeb8a74baa
scotow <contact@scotow.com>
2023-12-09 11:50:37 +0100

    Add 2023 day 09

--- 2023/src/bin/day_09.rs
+++ 2023/src/bin/day_09.rs
@@ -1,13 +1,33 @@
 advent_of_code_2023::main!();
 
-fn generator(input: &str) -> &str {
+fn generator(input: &str) -> Vec<Vec<i32>> {
     input
+        .lines()
+        .map(|l| l.split_whitespace().map(|n| n.parse().unwrap()).collect())
+        .collect()
 }
 
-fn part_1(input: &str) -> &str {
+fn part_1(input: Vec<Vec<i32>>) -> i32 {
     input
+        .into_iter()
+        .map(|n| solve(n, <[i32]>::last, i32::add))
+        .sum()
 }
 
-fn part_2(input: &str) -> &str {
+fn part_2(input: Vec<Vec<i32>>) -> i32 {
     input
+        .into_iter()
+        .map(|n| solve(n, <[i32]>::first, i32::sub))
+        .sum()
+}
+
+fn solve(input: Vec<i32>, side: fn(&[i32]) -> Option<&i32>, op: fn(i32, i32) -> i32) -> i32 {
+    if input.iter().all_equal() {
+        input[0]
+    } else {
+        op(
+            *side(&input).unwrap(),
+            solve(input.windows(2).map(|ns| ns[1] - ns[0]).collect(), side, op),
+        )
+    }
 }
