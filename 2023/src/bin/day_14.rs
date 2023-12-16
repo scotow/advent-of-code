72922344af02972e9d61aabc2f9d33a0dc30d6fe
scotow <contact@scotow.com>
2023-12-16 11:17:47 +0100

    Add 2023 day 14

--- 2023/src/bin/day_14.rs
+++ 2023/src/bin/day_14.rs
@@ -1,62 +1,40 @@
 advent_of_code_2023::main!();
 
 fn generator(input: &str) -> Vec<Vec<u8>> {
-    input.lines()
-        .map(|l| l.as_bytes().to_owned())
-        .collect()
+    input.lines().map(|l| l.as_bytes().to_owned()).collect()
 }
 
 fn part_1(mut input: Vec<Vec<u8>>) -> usize {
-    dbg!(&input);
     for (y, x) in iproduct!(0..input.len(), 0..input[0].len()) {
         if input[y][x] != b'O' {
             continue;
         }
-        // dbg!(x, y);
         for (ys, yd) in (0..=y).rev().tuple_windows() {
-            // dbg!(ys, yd);
-            // return 0;
             if input[yd][x] != b'.' {
                 break;
             }
             input[yd][x] = b'O';
             input[ys][x] = b'.';
         }
-
-        // break;
     }
-    for (y, x) in iproduct!(0..input.len(), 0..input[0].len()) {
-        if x == 0 {
-            println!();
-        }
-        print!("{}", input[y][x] as char);
-    }
-    println!();
-
     load(&input)
 }
 
 fn part_2(mut input: Vec<Vec<u8>>) -> usize {
     let mut cache = HashMap::<usize, Vec<(Vec<Vec<u8>>, usize)>>::new();
-
-    let mut i = 0;
-    loop {
+    for i in 1.. {
         // North.
         for (y, x) in iproduct!(0..input.len(), 0..input[0].len()) {
             if input[y][x] != b'O' {
                 continue;
             }
-            // dbg!(x, y);
             for (ys, yd) in (0..=y).rev().tuple_windows() {
-                // dbg!(ys, yd);
                 if input[yd][x] != b'.' {
                     break;
                 }
                 input[yd][x] = b'O';
                 input[ys][x] = b'.';
             }
-
-            // break;
         }
 
         // // West.
@@ -64,17 +42,13 @@ fn part_2(mut input: Vec<Vec<u8>>) -> usize {
             if input[y][x] != b'O' {
                 continue;
             }
-            // dbg!(x, y);
             for (xs, xd) in (0..=x).rev().tuple_windows() {
-                // dbg!(xs, xd);
                 if input[y][xd] != b'.' {
                     break;
                 }
                 input[y][xd] = b'O';
                 input[y][xs] = b'.';
             }
-
-            // break;
         }
 
         // South.
@@ -82,17 +56,13 @@ fn part_2(mut input: Vec<Vec<u8>>) -> usize {
             if input[y][x] != b'O' {
                 continue;
             }
-            // dbg!(x, y);
             for (ys, yd) in (y..=input.len() - 1).tuple_windows() {
-                // dbg!(ys, yd);
                 if input[yd][x] != b'.' {
                     break;
                 }
                 input[yd][x] = b'O';
                 input[ys][x] = b'.';
             }
-
-            // break;
         }
 
         // East
@@ -100,43 +70,30 @@ fn part_2(mut input: Vec<Vec<u8>>) -> usize {
             if input[y][x] != b'O' {
                 continue;
             }
-            // dbg!(x, y);
             for (xs, xd) in (x..=input[0].len() - 1).tuple_windows() {
-                // dbg!(xs, xd);
                 if input[y][xd] != b'.' {
                     break;
                 }
                 input[y][xd] = b'O';
                 input[y][xs] = b'.';
             }
-
-            // break;
         }
-        i += 1;
 
         let load = load(&input);
-        if let Some(prev) = cache.entry(load).or_default() {
-            if let Some(i) = prev.iter().find
-            // dbg!(i, prev == input);
-            // for (y, x) in iproduct!(0..input.len(), 0..input[0].len()) {
-            //     if x == 0 {
-            //         println!();
-            //     }
-            //     print!("{}", prev[y][x] as char);
-            // }
-            // println!();
+        let same_load = cache.entry(load).or_default();
+        match same_load
+            .iter()
+            .find_map(|(g, i)| (g == &input).then_some(i))
+        {
+            Some(j) => {
+                if (1_000_000_000 - i) % (i - j) == 0 {
+                    return load;
+                }
+            }
+            None => same_load.push((input.clone(), i)),
         }
     }
-
-    // for (y, x) in iproduct!(0..input.len(), 0..input[0].len()) {
-    //     if x == 0 {
-    //         println!();
-    //     }
-    //     print!("{}", input[y][x] as char);
-    // }
-    // println!();
-
-    0
+    unreachable!()
 }
 
 fn load(grid: &Vec<Vec<u8>>) -> usize {
@@ -145,4 +102,3 @@ fn load(grid: &Vec<Vec<u8>>) -> usize {
         .map(|(y, _)| grid.len() - y)
         .sum()
 }
-
