989e58224bdf72a8d80eff8013d0231a8c38425c
scotow <contact@scotow.com>
2023-12-17 21:09:10 +0100

    Optimize 2023 day 17

--- 2023/src/bin/day_17.rs
+++ 2023/src/bin/day_17.rs
@@ -8,63 +8,43 @@ fn generator(input: &str) -> Vec<Vec<u16>> {
 }
 
 fn part_1(input: Vec<Vec<u16>>) -> u16 {
-    solve(&input, |aligned, (x, y), (nx, ny), ps| {
-        match (aligned, ps.len()) {
-            (true, 3) => None,
-            (true, _) => Some((
-                ((nx, ny), chain!(ps.iter().copied(), Some((x, y))).collect()),
-                input[ny][nx],
-            )),
-            (false, _) => Some((((nx, ny), vec![(x, y)]), input[ny][nx])),
-        }
+    solve(&input, |aligned, n| match (aligned, n) {
+        (true, 3) => None,
+        (true, _) => Some(n + 1),
+        (false, _) => Some(1),
     })
 }
 
 fn part_2(input: Vec<Vec<u16>>) -> u16 {
-    solve(&input, |aligned, (x, y), (nx, ny), ps| {
-        match (aligned, ps.len()) {
-            (false, ..=3) | (true, 10..) => None,
-            (true, _) => Some((
-                ((nx, ny), chain!(ps.iter().copied(), Some((x, y))).collect()),
-                input[ny][nx],
-            )),
-            (false, _) => Some((((nx, ny), vec![(x, y)]), input[ny][nx])),
-        }
+    solve(&input, |aligned, n| match (aligned, n) {
+        (false, ..=3) | (true, 10..) => None,
+        (true, _) => Some(n + 1),
+        (false, _) => Some(1),
     })
 }
 
-fn solve<
-    F: Fn(bool, Pos<usize>, Pos<usize>, &[Pos<usize>]) -> Option<((Pos<usize>, Vec<Pos<usize>>), u16)>,
->(
-    input: &[Vec<u16>],
-    nf: F,
-) -> u16 {
+fn solve<F: Fn(bool, usize) -> Option<usize>>(input: &[Vec<u16>], nf: F) -> u16 {
     [(1, 0), (0, 1)]
         .into_iter()
         .map(|(sx, sy)| {
             dijkstra(
-                &((sx, sy), vec![(0, 0)]),
-                |((x, y), ps)| {
-                    neighbors4(*x, *y)
+                &((sx, sy), (0, 0), 0),
+                |&((x, y), (lx, ly), n)| {
+                    neighbors4(x, y)
                         .filter(|&(nx, ny)| {
-                            (nx, ny) != *ps.last().unwrap()
-                                && nx < input[0].len()
-                                && ny < input.len()
+                            (nx, ny) != (lx, ly) && nx < input[0].len() && ny < input.len()
                         })
                         .filter_map(|(nx, ny)| {
                             nf(
-                                (nx as isize - *x as isize)
-                                    == (*x as isize - ps.last().unwrap().0 as isize)
-                                    && (ny as isize - *y as isize)
-                                        == (*y as isize - ps.last().unwrap().1 as isize),
-                                (*x, *y),
-                                (nx, ny),
-                                &ps,
+                                (nx as isize - x as isize) == (x as isize - lx as isize)
+                                    && (ny as isize - y as isize) == (y as isize - ly as isize),
+                                n,
                             )
+                            .map(|n| (((nx, ny), (x, y), n), input[ny][nx]))
                         })
                         .collect::<Vec<_>>()
                 },
-                |(xy, _)| *xy == (input[0].len() - 1, input.len() - 1),
+                |(xy, _, _)| *xy == (input[0].len() - 1, input.len() - 1),
             )
             .unwrap()
             .1 + input[sy][sx]
