42ae09ac6990337558c359c40dbcc7f4858bdcb3
scotow <contact@scotow.com>
2023-12-17 22:26:12 +0100

    Optimize 2023 day 17 x2

--- 2023/src/bin/day_17.rs
+++ 2023/src/bin/day_17.rs
@@ -8,22 +8,14 @@ fn generator(input: &str) -> Vec<Vec<u16>> {
 }
 
 fn part_1(input: Vec<Vec<u16>>) -> u16 {
-    solve(&input, |aligned, n| match (aligned, n) {
-        (true, 3) => None,
-        (true, _) => Some(n + 1),
-        (false, _) => Some(1),
-    })
+    solve(&input, 0, 3)
 }
 
 fn part_2(input: Vec<Vec<u16>>) -> u16 {
-    solve(&input, |aligned, n| match (aligned, n) {
-        (false, ..=3) | (true, 10..) => None,
-        (true, _) => Some(n + 1),
-        (false, _) => Some(1),
-    })
+    solve(&input, 3, 10)
 }
 
-fn solve<F: Fn(bool, usize) -> Option<usize>>(input: &[Vec<u16>], nf: F) -> u16 {
+fn solve(input: &[Vec<u16>], min: usize, max: usize) -> u16 {
     [(1, 0), (0, 1)]
         .into_iter()
         .map(|(sx, sy)| {
@@ -31,15 +23,17 @@ fn solve<F: Fn(bool, usize) -> Option<usize>>(input: &[Vec<u16>], nf: F) -> u16 
                 &((sx, sy), (0, 0), 0),
                 |&((x, y), (lx, ly), n)| {
                     neighbors4(x, y)
-                        .filter(|&(nx, ny)| {
-                            (nx, ny) != (lx, ly) && nx < input[0].len() && ny < input.len()
-                        })
                         .filter_map(|(nx, ny)| {
-                            nf(
-                                (nx as isize - x as isize) == (x as isize - lx as isize)
-                                    && (ny as isize - y as isize) == (y as isize - ly as isize),
-                                n,
-                            )
+                            if (nx, ny) == (lx, ly) || nx >= input[0].len() || ny >= input.len() {
+                                return None;
+                            }
+                            match (nx as isize - x as isize) == (x as isize - lx as isize)
+                                && (ny as isize - y as isize) == (y as isize - ly as isize)
+                            {
+                                true if n < max => Some(n + 1),
+                                false if n > min => Some(1),
+                                _ => None,
+                            }
                             .map(|n| (((nx, ny), (x, y), n), input[ny][nx]))
                         })
                         .collect::<Vec<_>>()
